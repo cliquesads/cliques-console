9f3869c405f510fe5b511009d420b2cc0895744e
Ben Liang <bliang@cliquesads.com>
2015-07-08 16:34:31 -0400

    NF: Fixed advertiser menu item

--- public/modules/advertiser/config/advertisers.client.config.js
+++ public/modules/advertiser/config/advertisers.client.config.js
@@ -3,6 +3,6 @@
 angular.module('advertiser').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-        Menus.addMenuItem('sidebar', 'Advertiser', 'advertiser', null, '/advertiser', true, null, null, 'icon-home');
+        Menus.addMenuItem('sidebar', 'Advertiser', 'advertiser', null, '/advertiser', false, ['admin','advertiser'], 2, 'icon-bar-chart');
 	}
 ]);
