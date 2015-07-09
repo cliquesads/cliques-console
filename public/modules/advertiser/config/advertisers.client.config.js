1a22aaec779369f6b074b92b0f17a47bc7b142d0
Ben Liang <bliang@cliquesads.com>
2015-07-09 14:01:27 -0400

    NF: fix advertiser menu item

--- public/modules/advertiser/config/advertisers.client.config.js
+++ public/modules/advertiser/config/advertisers.client.config.js
@@ -3,6 +3,7 @@
 angular.module('advertiser').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-        Menus.addMenuItem('sidebar', 'Advertiser', 'advertiser', null, '/advertiser', false, ['admin','advertiser'], 2, 'icon-bar-chart');
+        Menus.addMenuItem('sidebar', 'Advertisers', 'advertiser', null, '/advertiser', false, ['admin','advertiser'], 2, 'icon-bar-chart');
+        Menus.addSubMenuItem('sidebar', 'advertiser', 'New Advertiser', 'advertiser/create');
 	}
 ]);
