f6a2d360bd277daac284f0ff034306aea511e89b
Ben Liang <bliang@cliquesads.com>
2015-07-15 16:58:32 -0400

    RF: Refactored out tree-generation code into service, rather than root Cliques controller

--- public/modules/clique/controllers/cliques.client.controller.js
+++ public/modules/clique/controllers/cliques.client.controller.js
@@ -1,65 +1,13 @@
 'use strict';
 
-/**
- * Formats Cliques query result for ABN-tree plugin
- *
- * @param query_result
- * @returns {*}
- */
-function format_cliques_collection_for_tree(query_result){
-    /**
-     * Converts Clique collection into tree-structure object
-     */
-    function _get_or_create_branch(obj, branch_arr){
-        if (branch_arr.length > 0){
-            var child = branch_arr.shift();
-            if (!obj.hasOwnProperty(child)){
-                obj[child] = {};
-            }
-            obj = obj[child];
-            return _get_or_create_branch(obj,branch_arr);
-        }
-    }
-    /**
-     * Recursive method to convert tree object into abn-formatted tree array
-     */
-    function _reformat_tree(cliques_tree, _destination_array){
-        _destination_array = _destination_array || [];
-        if (cliques_tree != {}){
-            for (var key in cliques_tree){
-                if (cliques_tree.hasOwnProperty(key)){
-                    var obj = {
-                        label: key,
-                        children: []
-                    };
-                    _reformat_tree(cliques_tree[key], obj.children);
-                    _destination_array.push(obj);
-
-                }
-            }
-        }
-        return _destination_array;
-    }
-
-    var cliques = {};
-    query_result.forEach(function(clique){
-        var ancestors = clique.ancestors;
-        ancestors.push(clique._id);
-        _get_or_create_branch(cliques, ancestors);
-    });
-    return _reformat_tree(cliques);
-}
-
-angular.module('clique').controller('CliqueController', ['$scope', '$stateParams', '$location', 'Authentication', 'Clique',
-	function($scope, $stateParams, $location, Authentication, Clique) {
+angular.module('clique').controller('CliqueController', ['$scope', '$stateParams', '$location', 'Authentication', 'Clique','getCliqueTree',
+	function($scope, $stateParams, $location, Authentication, Clique, getCliqueTree) {
 		$scope.authentication = Authentication;
 
         // Populate tree data for tree visualization
         $scope.cliques = [];
 		$scope.find = function() {
-            var query_result = Clique.query(function(){
-                $scope.cliques = format_cliques_collection_for_tree(query_result);
-            });
+            getCliqueTree($scope);
 		};
 
         $scope.my_tree_handler = function(branch) {
--- public/modules/clique/services/cliques.client.service.js
+++ public/modules/clique/services/cliques.client.service.js
@@ -1,5 +1,54 @@
 'use strict';
 
+/**
+ * Formats Cliques query result for ABN-tree plugin
+ *
+ * @param query_result
+ * @returns {*}
+ */
+function format_cliques_collection_for_tree(query_result){
+    /**
+     * Converts Clique collection into tree-structure object
+     */
+    function _get_or_create_branch(obj, branch_arr){
+        if (branch_arr.length > 0){
+            var child = branch_arr.shift();
+            if (!obj.hasOwnProperty(child)){
+                obj[child] = {};
+            }
+            obj = obj[child];
+            return _get_or_create_branch(obj,branch_arr);
+        }
+    }
+    /**
+     * Recursive method to convert tree object into abn-formatted tree array
+     */
+    function _reformat_tree(cliques_tree, _destination_array){
+        _destination_array = _destination_array || [];
+        if (cliques_tree != {}){
+            for (var key in cliques_tree){
+                if (cliques_tree.hasOwnProperty(key)){
+                    var obj = {
+                        label: key,
+                        children: []
+                    };
+                    _reformat_tree(cliques_tree[key], obj.children);
+                    _destination_array.push(obj);
+
+                }
+            }
+        }
+        return _destination_array;
+    }
+    var cliques = {};
+    query_result.forEach(function(clique){
+        var ancestors = clique.ancestors;
+        ancestors.push(clique._id);
+        _get_or_create_branch(cliques, ancestors);
+    });
+    return _reformat_tree(cliques);
+}
+
 //Articles service used for communicating with the articles REST endpoints
 angular.module('clique').factory('Clique', ['$resource',
 	function($resource) {
@@ -11,4 +60,15 @@ angular.module('clique').factory('Clique', ['$resource',
 		    }
         );
 	}
+]).
+factory('getCliqueTree', ['Clique', function(Clique){
+        return function(scope){
+            Clique.query(function(){
+                // Populate tree data for tree visualization
+                var query_result = Clique.query(function(){
+                    scope.cliques = format_cliques_collection_for_tree(query_result);
+                });
+            })
+        }
+    }
 ]);
