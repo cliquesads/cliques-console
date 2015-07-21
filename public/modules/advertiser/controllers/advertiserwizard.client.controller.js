a7a5b8d452ef51cc8b0ce1c0dae71b40eee13373
Ben Liang <bliang@cliquesads.com>
2015-07-20 23:50:53 -0400

    NF: Beginnings of creative upload step

--- public/modules/advertiser/controllers/advertiserwizard.client.controller.js
+++ public/modules/advertiser/controllers/advertiserwizard.client.controller.js
@@ -1,7 +1,15 @@
 'use strict';
 
-angular.module('advertiser').controller('AdvertiserWizardController', ['$scope', '$stateParams', '$location', 'Authentication', 'Advertiser','DatepickerService','getCliqueTree', 'DMA',
-	function($scope, $stateParams, $location, Authentication, Advertiser, DatepickerService, getCliqueTree, DMA) {
+angular.module('advertiser').controller('AdvertiserWizardController', ['$scope',
+    '$stateParams',
+    '$location',
+    'Authentication',
+    'Advertiser',
+    'DatepickerService',
+    'getCliqueTree',
+    'DMA',
+    'FileUploader',
+	function($scope, $stateParams, $location, Authentication, Advertiser, DatepickerService, getCliqueTree, DMA, FileUploader) {
 		$scope.authentication = Authentication;
         $scope.calendar = DatepickerService;
         $scope.Math = Math;
@@ -117,5 +125,61 @@ angular.module('advertiser').controller('AdvertiserWizardController', ['$scope',
             var input = this.advertiserForm[name];
             return (input.$dirty || $scope.submitted) && input.$error[type];
         };
+
+
+        //#################################
+        //######### FILE UPLOADER #########
+        //#################################
+
+        var uploader = $scope.uploader = new FileUploader({
+            url: 'uploads'
+        });
+
+        // FILTERS
+
+        uploader.filters.push({
+            name: 'customFilter',
+            fn: function(item /*{File|FileLikeObject}*/, options) {
+                return this.queue.length < 10;
+            }
+        });
+
+        // CALLBACKS
+
+        uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
+            console.info('onWhenAddingFileFailed', item, filter, options);
+        };
+        uploader.onAfterAddingFile = function(fileItem) {
+            console.info('onAfterAddingFile', fileItem);
+        };
+        uploader.onAfterAddingAll = function(addedFileItems) {
+            console.info('onAfterAddingAll', addedFileItems);
+        };
+        uploader.onBeforeUploadItem = function(item) {
+            console.info('onBeforeUploadItem', item);
+        };
+        uploader.onProgressItem = function(fileItem, progress) {
+            console.info('onProgressItem', fileItem, progress);
+        };
+        uploader.onProgressAll = function(progress) {
+            console.info('onProgressAll', progress);
+        };
+        uploader.onSuccessItem = function(fileItem, response, status, headers) {
+            console.info('onSuccessItem', fileItem, response, status, headers);
+        };
+        uploader.onErrorItem = function(fileItem, response, status, headers) {
+            console.info('onErrorItem', fileItem, response, status, headers);
+        };
+        uploader.onCancelItem = function(fileItem, response, status, headers) {
+            console.info('onCancelItem', fileItem, response, status, headers);
+        };
+        uploader.onCompleteItem = function(fileItem, response, status, headers) {
+            console.info('onCompleteItem', fileItem, response, status, headers);
+        };
+        uploader.onCompleteAll = function() {
+            console.info('onCompleteAll');
+        };
+
+        console.info('uploader', uploader);
 	}
 ]);
