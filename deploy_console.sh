ee71826f310702e462da049e4b1dff90b1b35996
Ben Liang <bliang@cliquesads.com>
2015-07-24 13:20:12 -0400

    NF: Activate & deploy scripts

---
+++ deploy_console.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source ./activate_production.sh
+nvm use 0.12.0
+npm install
+grunt build
+
+processname='cliques-console'
+running=$(pm2 list -m | grep "$processname")
+
+if [ -z "$running" ]; then
+#    # hook PM2 up to web monitoring with KeyMetrics
+#    pm2 link d39yzaslt8iu57e w77ttxdzer9p8zv $hostname
+    # start in cluster mode
+    pm2 start server.js --name "$processname" -i 0
+else
+    pm2 gracefulReload "$processname"
+fi
