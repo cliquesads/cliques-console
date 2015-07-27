cdfd99e7766fb49dc109d8c2d3c7e808c7ee077b
Ben Liang <bliang@cliquesads.com>
2015-07-27 13:56:24 -0400

    BF: Have to include jquery.flot.js before all other plugin files

--- config/env/all.js
+++ config/env/all.js
@@ -46,7 +46,8 @@ module.exports = {
                 'public/lib/angular-file-upload/angular-file-upload.min.js',
                 'public/lib/bootstrap-filestyle/src/bootstrap-filestyle.js',
                 'public/lib/angular-xeditable/dist/js/xeditable.min.js',
-                'public/lib/flot/jquery.flot.js'
+                'public/lib/flot/jquery.flot.js',
+                'public/lib/flot/jquery.flot.*.js'
 			]
 		},
 		css: [
