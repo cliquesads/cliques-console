e8c1e2f398f9bb4485386f61540b353d6121d78e
Ben Liang <bliang@cliquesads.com>
2015-08-01 13:22:37 -0400

    NF: HourlyAdStats Angular service to query endpoint

---
+++ public/modules/aggregations/services/aggregations.client.service.js
@@ -0,0 +1,35 @@
+'use strict';
+
+//Articles service used for communicating with the articles REST endpoints
+angular.module('advertiser').factory('HourlyAdStat', ['$http',
+	function($http) {
+        var base_path    = '/hourlyadstat';
+        var adv_path     = base_path + '/adv';
+        var pub_path     = base_path + '/pub';
+        var clique_path  = base_path + 'clique';
+
+        var hourlyadstatfactory = {};
+
+        // Wrapper for query function so you don't have to
+        // duplicate advertiser & publisher code
+        function constructQueryFunc(hierarchy, basepath){
+            return function(idsObject, queryParams){
+                var relative_path = '';
+                // constructs path param
+                hierarchy.forEach(function(key){
+                    if (idsObject.hasOwnProperty(key)){
+                        relative_path += '/' + idsObject[key];
+                    }
+                });
+                var path = basepath + relative_path;
+                return $http.get(path, {params: queryParams})
+            }
+        }
+
+        hourlyadstatfactory.advQuery = constructQueryFunc(['advertiserId', 'campaignId', 'creativegroupId','creativeId'], adv_path);
+        hourlyadstatfactory.pubQuery = constructQueryFunc(['publisherId', 'siteId', 'pageId','placementId'], pub_path);
+        hourlyadstatfactory.cliqueQuery = function(queryParams){
+            return $http.get(clique_path, queryParams);
+        }
+	}
+]);
