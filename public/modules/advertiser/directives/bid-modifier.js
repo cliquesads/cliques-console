6203029d7a97dddfa424ef5d89469ce096039e63
Ben Liang <bliang@cliquesads.com>
2015-08-16 11:43:35 -0400

    RF: Begin pretty large refactor to move all campaign UI components to directives

---
+++ public/modules/advertiser/directives/bid-modifier.js
@@ -0,0 +1,21 @@
+/**
+ * Created by bliang on 8/15/15.
+ */
+angular.module('advertiser').directive('bidModifier', [function() {
+    'use strict';
+    return {
+        restrict: 'E',
+        scope: {
+            title: '@',
+            targets: '=',
+            options: '=',
+            campaign: '=',
+            wizard_step: '@',
+            width: '@'
+        },
+        templateUrl: 'modules/advertiser/views/partials/bid-modifier.html',
+        link: function(scope, element, attrs){
+            scope.div_id = scope.title.toLowerCase();
+        }
+    };
+}]);
