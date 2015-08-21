34601ba54fc4f86c3396aa6bf7af7d2d47a0f88b
Ben Liang <bliang@cliquesads.com>
2015-08-21 02:45:19 -0400

    NF: Bunch of fixes & more progress on the uploader, almost ready

--- public/modules/advertiser/controllers/editCreatives.client.controller.js
+++ public/modules/advertiser/controllers/editCreatives.client.controller.js
@@ -26,6 +26,12 @@ angular.module('advertiser').controller('editCreativesController', [
         //this way, all Advertiser resource methods will work
         $scope.campaign = $scope.advertiser.campaigns[i];
 
+        $scope.update = function(){
+            this.advertiser.$update(function(){
+            },function(errorResponse){
+                $scope.saveerror = errorResponse.data.message;
+            });
+        };
 
         //#################################//
         //######### FILE UPLOADER #########//
@@ -35,8 +41,30 @@ angular.module('advertiser').controller('editCreativesController', [
             url: 'creativeassets'
         });
         $scope.uploader.onCompleteAll = function(){
-            $scope.uploads_completed = true;
+            // When all uploads are complete, modify advertiser object for new creatives and call $update
+            var creatives = AdvertiserUtils.getCreativesFromUploadQueue($scope.uploader);
+            var creativegroups = AdvertiserUtils.groupCreatives(creatives, $scope.campaign.name);
+            // have to merge new creativeGroups with any existing first
+            $scope.$apply(function() {
+                creativegroups.forEach(function (crg) {
+                    var ind = _.findIndex($scope.campaign.creativegroups, function (cg) {
+                        return cg.w === crg.w && cg.h === crg.h
+                    });
+                    // if creativegroup of same size exists, add to this creative group
+                    if (ind > -1) {
+                        $scope.campaign.creativegroups[ind] = _.union($scope.campaign.creativegroups[ind], crg);
+                    } else {
+                        $scope.campaign.creativegroups.push(crg);
+                    }
+                });
+                $scope.advertiser.$update(function(){
+                    $scope.uploader.clearQueue();
+                }, function(errorResponse){
+                    $scope.saveerror = errorResponse.data.message;
+                });
+            });
         };
+
         /**
          * Wrapper for uploader.uploadAll() which allows form to pass
          * validation function to call first.
@@ -51,38 +79,39 @@ angular.module('advertiser').controller('editCreativesController', [
             }
         };
 
-        $scope.update = function(){
-            this.advertiser.$update(function(){
-            },function(errorResponse){
-               $scope.saveerror = errorResponse.data.message;
-            });
-        };
-
         // Add remove() method to each creative
-        $scope.campaign.creativegroups.forEach(function(creativegroup){
-            creativegroup.creatives.forEach(function(creative){
-                // remove function
-                creative.remove = function(){
-                    ngDialog.openConfirm({
-                        template:'\
+        $scope.$watch(function(scope){ return scope.campaign }, function(){
+            $scope.campaign.creativegroups.forEach(function(creativegroup){
+                creativegroup.creatives.forEach(function(creative){
+                    // remove function
+                    creative.remove = function(){
+                        ngDialog.openConfirm({
+                            template:'\
                             <p>Are you sure you want to delete this creative? This cannot be undone.</p>\
                             <div class="ngdialog-buttons">\
                                 <button type="button" class="ngdialog-button ngdialog-button-secondary" ng-click="closeThisDialog(0)">No</button>\
                                 <button type="button" class="ngdialog-button ngdialog-button-primary" ng-click="confirm(1)">Yes</button>\
                         </div>',
-                        plain: true
-                    }).then(function(val){
-                        var ind = creativegroup.creatives.indexOf(creative);
-                        creativegroup.creatives.splice(ind,1);
-                        if (creativegroup.creatives.length === 0){
-                            var crgind = $scope.campaign.creativegroups.indexOf(creativegroup);
-                            $scope.campaign.creativegroups.splice(crgind,1);
-                            $scope.update();
-                        }
-                    });
-                }
+                            plain: true
+                        }).then(function(val){
+                            var ind = creativegroup.creatives.indexOf(creative);
+                            creativegroup.creatives.splice(ind,1);
+                            if (creativegroup.creatives.length === 0){
+                                var crgind = $scope.campaign.creativegroups.indexOf(creativegroup);
+                                $scope.campaign.creativegroups.splice(crgind,1);
+                                $scope.update();
+                            }
+                        });
+                    }
+                });
             });
         });
+
+
+        $scope.validateQueue = function(){
+            //TODO: This is pretty janky
+            return $('#creativeUploadQueue').parsley().validate();
+        };
 
         $scope.updateAndClose = function(){
             this.advertiser.$update(function() {
