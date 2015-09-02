484f47f29c44161f16b25a4d3902c1d222448afc
Ben Liang <bliang@cliquesads.com>
2015-09-02 15:09:38 -0400

    NF: placementTag service

---
+++ public/modules/publisher/services/placements.client.service.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('publisher').factory('PlacementTag', ['$http',
+    function($http) {
+        var placementTagFactory = {};
+        placementTagFactory.getTag = function(params) {
+            var path = '/publisher/'+params.publisherId+'placement'+params.placementId;
+            var queryParams = { secure: params.secure || false };
+            return $http.get(path, {params: queryParams});
+        };
+        return placementTagFactory;
+    }
+]);
