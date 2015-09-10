ff3d3335c8f33dda358104a1a76eb2b057ad13ba
Ben Liang <bliang@cliquesads.com>
2015-09-10 01:28:56 -0400

    RF: Trying something different

--- https_redirect.js
+++ https_redirect.js
@@ -6,14 +6,9 @@ var redirectServer = http.createServer(redirectApp);
 var GCLB_USERAGENT = new RegExp('^Google-GCLB.*');
 
 redirectApp.use(function requireHTTPS(req, res, next) {
-    if (GCLB_USERAGENT.exec(req.headers['user-agent'])){
-        console.log('GCLB Request - Status 200');
-        res.status(200).send();
-    } else {
-        if (!req.secure) {
-            return res.redirect('https://' + req.headers.host + req.url);
-        }
-        next();
+    if (!req.secure) {
+        return res.redirect('https://' + req.headers.host + req.url);
     }
+    next();
 });
-redirectServer.listen(8080);
+redirectServer.listen(3000);
