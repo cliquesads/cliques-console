ad3e65248bae7dc78530cd88f29930141c34b25b
Ben Liang <bliang@cliquesads.com>
2015-09-10 00:39:30 -0400

    NF: http to https redirect simple app

---
+++ https_redirect.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var http = require('http');
+var redirectApp = express();
+var redirectServer = http.createServer(redirectApp);
+
+redirectApp.use(function requireHTTPS(req, res, next) {
+    if (!req.secure) {
+        return res.redirect('https://' + req.headers.host + req.url);
+    }
+    next();
+});
+redirectServer.listen(8080);
