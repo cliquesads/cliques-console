708d20b5755e80c013fc731be0f31cb2dd92b7b8
Ben Liang <bliang@cliquesads.com>
2015-09-10 09:52:09 -0400

    NF: Change to use secureURL

--- https_redirect.js
+++ https_redirect.js
@@ -3,8 +3,6 @@ var http = require('http');
 var redirectApp = express();
 var redirectServer = http.createServer(redirectApp);
 
-var GCLB_USERAGENT = new RegExp('^Google-GCLB.*');
-
 redirectApp.use(function requireHTTPS(req, res, next) {
     if (!req.secure) {
         return res.redirect('https://' + req.headers.host + req.url);
