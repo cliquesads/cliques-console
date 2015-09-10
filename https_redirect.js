86e11641d9d35c90eebe6fd3dbdfb82f746a4a5d
Ben Liang <bliang@cliquesads.com>
2015-09-10 01:19:50 -0400

    NF: Have to filter requests by UA & respond to load balancer requests separately

--- https_redirect.js
+++ https_redirect.js
@@ -3,10 +3,17 @@ var http = require('http');
 var redirectApp = express();
 var redirectServer = http.createServer(redirectApp);
 
+var GCLB_USERAGENT = new RegExp('^Google-GCLB.*');
+
 redirectApp.use(function requireHTTPS(req, res, next) {
-    if (!req.secure) {
-        return res.redirect('https://' + req.headers.host + req.url);
+    if (GCLB_USERAGENT.exec(req.headers['user-agent'])){
+        console.log('GCLB Request - Status 200');
+        res.status(200).send();
+    } else {
+        if (!req.secure) {
+            return res.redirect('https://' + req.headers.host + req.url);
+        }
+        next();
     }
-    next();
 });
 redirectServer.listen(8080);
