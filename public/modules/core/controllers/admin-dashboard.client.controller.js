f2c1858fd884d0dbf739609195095c3b424f1c42
Ben Liang <bliang@cliquesads.com>
2015-10-08 00:25:03 -0400

    NF: Lots of changes to admin dash

--- public/modules/core/controllers/admin-dashboard.client.controller.js
+++ public/modules/core/controllers/admin-dashboard.client.controller.js
@@ -6,8 +6,8 @@
 'use strict';
 
 angular.module('core').controller('AdminDashboardController',
-    ['$scope','$location','$window','Advertiser','Publisher','HourlyAdStat','MongoTimeSeries','aggregationDateRanges','Authentication',
-        function($scope, $location, $window, Advertiser, Publisher,HourlyAdStat, MongoTimeSeries, aggregationDateRanges, Authentication) {
+    ['$scope','$location','$window','Advertiser','Publisher','DTOptionsBuilder','DTColumnDefBuilder','HourlyAdStat','MongoTimeSeries','aggregationDateRanges','Authentication',
+        function($scope, $location, $window, Advertiser, Publisher, DTOptionsBuilder, DTColumnDefBuilder, HourlyAdStat, MongoTimeSeries, aggregationDateRanges, Authentication) {
             $scope.creatives = [];
             $scope.advertisers = Advertiser.query(function(advertisers){
                 advertisers.forEach(function(adv){
@@ -15,11 +15,13 @@ angular.module('core').controller('AdminDashboardController',
                         camp.creativegroups.forEach(function(crg){
                             crg.creatives.forEach(function(cr){
                                 var description = camp.name + ' - ' + cr.name;
+                                var link = '#!/advertiser/' + adv._id + '/campaign/' + camp._id;
                                 $scope.creatives.push({
                                     id: cr._id,
                                     title: adv.name,
                                     description: description,
-                                    src: cr.url
+                                    src: cr.url,
+                                    link: link
                                 });
                             });
                         });
@@ -33,11 +35,10 @@ angular.module('core').controller('AdminDashboardController',
             });
 
             // See service in aggregations module for details on aggregationDateRanges object
-            $scope.dateRangeSelection = "7d";
+            $scope.summaryDateRangeSelection = "7d";
             $scope.dateRanges = aggregationDateRanges(user.tz);
-
             $scope.getDashboardGraph = function(dateShortCode) {
-                dateShortCode = dateShortCode || $scope.dateRangeSelection;
+                dateShortCode = dateShortCode || $scope.summaryDateRangeSelection;
                 var startDate = $scope.dateRanges[dateShortCode].startDate;
                 var endDate = $scope.dateRanges[dateShortCode].endDate;
 
@@ -56,15 +57,114 @@ angular.module('core').controller('AdminDashboardController',
                 }).then(function (response) {
                     $scope.timeSeries = new MongoTimeSeries(response.data, startDate, endDate, user.tz, timeUnit,
                         {
-                            fields: ['imps', {
-                                'CTR': function (row) {
-                                    return row.clicks / row.imps;
-                                }
-                            }, 'clicks', 'spend']
+                            fields: [
+                                'imps',
+                                {'CTR':
+                                    function (row) {
+                                        return row.clicks / row.imps;
+                                    }
+                                },
+                                'clicks',
+                                'spend']
                         });
                 });
                 // TODO: Need to provide error callback for query promise as well
+                $scope.summaryDateRangeSelection = dateShortCode;
+            };
 
+            $scope.dateRangeSelection = "7d";
+            $scope.tabFunctions = {
+                cliques: function(dateShortCode){
+                    var startDate = $scope.dateRanges[dateShortCode].startDate;
+                    var endDate = $scope.dateRanges[dateShortCode].endDate;
+                    // query HourlyAdStats api endpoint
+                    HourlyAdStat.query({
+                        groupBy: 'pub_clique',
+                        startDate: startDate,
+                        endDate: endDate
+                    }).then(function(response){
+                        // build datatables options object
+                        $scope.dtOptions = DTOptionsBuilder.newOptions();
+                        $scope.dtOptions.withOption('paging', false);
+                        $scope.dtOptions.withOption('searching', false);
+                        $scope.dtOptions.withOption('scrollX', true);
+                        $scope.dtOptions.withOption('order', [[1,'desc']]);
+                        // Not entirely sure if this is necessary
+                        $scope.dtColumnDefs = [
+                            DTColumnDefBuilder.newColumnDef(0),
+                            DTColumnDefBuilder.newColumnDef(1),
+                            DTColumnDefBuilder.newColumnDef(2),
+                            DTColumnDefBuilder.newColumnDef(3),
+                            DTColumnDefBuilder.newColumnDef(4),
+                            DTColumnDefBuilder.newColumnDef(5)
+                        ];
+                        $scope.cliqueData = response.data;
+                    });
+                },
+                publishers: function(dateShortCode){
+                    var startDate = $scope.dateRanges[dateShortCode].startDate;
+                    var endDate = $scope.dateRanges[dateShortCode].endDate;
+                    // query HourlyAdStats api endpoint
+                    HourlyAdStat.query({
+                        groupBy: 'publisher',
+                        populate: 'publisher',
+                        startDate: startDate,
+                        endDate: endDate
+                    }).then(function(response) {
+                        // build datatables options object
+                        $scope.dtOptions_pubs = DTOptionsBuilder.newOptions();
+                        $scope.dtOptions_pubs.withOption('paging', false);
+                        $scope.dtOptions_pubs.withOption('searching', false);
+                        $scope.dtOptions_pubs.withOption('scrollX', true);
+                        $scope.dtOptions_pubs.withOption('order', [[2, 'desc']]);
+                        // Not entirely sure if this is necessary
+                        $scope.dtColumnDefs_pubs = [
+                            DTColumnDefBuilder.newColumnDef(0),
+                            DTColumnDefBuilder.newColumnDef(1),
+                            DTColumnDefBuilder.newColumnDef(2),
+                            DTColumnDefBuilder.newColumnDef(3),
+                            DTColumnDefBuilder.newColumnDef(4),
+                            DTColumnDefBuilder.newColumnDef(5),
+                            DTColumnDefBuilder.newColumnDef(6)
+                        ];
+                        $scope.publisherData = response.data;
+                    });
+                },
+                advertisers: function(dateShortCode){
+                    var startDate = $scope.dateRanges[dateShortCode].startDate;
+                    var endDate = $scope.dateRanges[dateShortCode].endDate;
+                    // query HourlyAdStats api endpoint
+                    HourlyAdStat.query({
+                        groupBy: 'advertiser',
+                        populate: 'advertiser',
+                        startDate: startDate,
+                        endDate: endDate
+                    }).then(function(response) {
+                        // build datatables options object
+                        $scope.dtOptions_advs = DTOptionsBuilder.newOptions();
+                        $scope.dtOptions_advs.withOption('paging', false);
+                        $scope.dtOptions_advs.withOption('searching', false);
+                        $scope.dtOptions_advs.withOption('scrollX', true);
+                        $scope.dtOptions_advs.withOption('order', [[2, 'desc']]);
+                        // Not entirely sure if this is necessary
+                        $scope.dtColumnDefs_advs = [
+                            DTColumnDefBuilder.newColumnDef(0),
+                            DTColumnDefBuilder.newColumnDef(1),
+                            DTColumnDefBuilder.newColumnDef(2),
+                            DTColumnDefBuilder.newColumnDef(3),
+                            DTColumnDefBuilder.newColumnDef(4),
+                            DTColumnDefBuilder.newColumnDef(5),
+                            DTColumnDefBuilder.newColumnDef(6)
+                        ];
+                        $scope.advertiserData = response.data;
+                    });
+                }
+            };
+            $scope.activeTab = 'cliques';
+            $scope.getTabData = function(dateShortCode, tab){
+                tab = tab || $scope.activeTab;
+                $scope.activeTab = tab;
+                $scope.tabFunctions[tab](dateShortCode);
                 $scope.dateRangeSelection = dateShortCode;
             };
         }
