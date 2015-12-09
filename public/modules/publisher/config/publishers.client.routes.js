d6a23d44417428cc3daeececca61ad8786a521b9
Ben Liang <bliang@cliquesads.com>
2015-12-09 02:29:10 -0500

    RF: Same refactor for publisher views & router as well

--- public/modules/publisher/config/publishers.client.routes.js
+++ public/modules/publisher/config/publishers.client.routes.js
@@ -5,35 +5,77 @@ angular.module('publisher').config(['$stateProvider',
 	function($stateProvider) {
 		// Publisher state routing
 		$stateProvider.
-		state('app.listPublishers', {
+        state('app.publisher', {
+            title: 'Publisher',
+            abstract: true,
+            templateUrl: 'modules/publisher/views/publisher-layout.client.view.html'
+        }).
+		state('app.publisher.listPublishers', {
 			url: '/publisher',
 			title: 'List Publishers',
-			templateUrl: 'modules/publisher/views/list-publisher.client.view.html',
-            controller: 'PublisherController'
+            views: {
+                'main': {
+                    templateUrl: 'modules/publisher/views/list-publisher.client.view.html',
+                    controller: 'PublisherController'
+                },
+                'titleBar': {
+                    template: 'Publishers'
+                }
+            }
 		}).
-		state('app.createPublisher', {
+		state('app.publisher.createPublisher', {
 			url: '/publisher/create',
 			title: 'New Publisher',
-			templateUrl: 'modules/publisher/views/create-publisher.client.view.html',
-            controller: 'PublisherWizardController'
+            views: {
+                'main': {
+                    templateUrl: 'modules/publisher/views/create-publisher.client.view.html',
+                    controller: 'PublisherWizardController'
+                },
+                'titleBar': {
+                    template: 'New Publisher'
+                }
+            }
 		}).
-        state('app.listSite', {
+        state('app.publisher.listSite', {
             url: '/publisher/site',
             title: 'List Sites',
-            templateUrl: 'modules/publisher/views/list-sites.client.view.html',
-            controller: 'SiteController'
+            views: {
+                'main': {
+                    templateUrl: 'modules/publisher/views/list-sites.client.view.html',
+                    controller: 'SiteController'
+                },
+                'titleBar': {
+                    template: 'Sites'
+                }
+            }
         }).
-		state('app.viewPublisher', {
+		state('app.publisher.viewPublisher', {
 			url: '/publisher/:publisherId',
 			title: 'View Publisher',
-			templateUrl: 'modules/publisher/views/view-publisher.client.view.html',
-			controller: 'PublisherController'
+            views: {
+                'main': {
+                    templateUrl: 'modules/publisher/views/view-publisher.client.view.html',
+                    controller: 'PublisherController'
+                },
+                'titleBar': {
+                    templateUrl: 'modules/publisher/views/partials/titlebar/view-publisher.titlebar.html',
+                    controller: 'PublisherController'
+                }
+            }
 		}).
-        state('app.viewSite', {
-            url: '/publisher/:publisherId/site/:siteId',
+        state('app.publisher.viewPublisher.viewSite', {
+            url: '/site/:siteId',
             title: 'View Site',
-            templateUrl: 'modules/publisher/views/view-site.client.view.html',
-            controller: 'SiteController'
+            views: {
+                'main': {
+                    templateUrl: 'modules/publisher/views/view-site.client.view.html',
+                    controller: 'SiteController'
+                },
+                'titleBar': {
+                    templateUrl: 'modules/publisher/views/partials/titlebar/view-site.titlebar.html',
+                    controller: 'SiteController'
+                }
+            }
         });
 	}
 ]);
