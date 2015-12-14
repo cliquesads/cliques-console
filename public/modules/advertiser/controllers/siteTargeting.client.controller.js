0c9c760665fdd9b4cd3063de8211fe19cb1c464e
Ben Liang <bliang@cliquesads.com>
2015-12-14 12:40:36 -0500

    NF: Some bullshit to restructure yet another tree yet another way for the lib I'm using

--- public/modules/advertiser/controllers/siteTargeting.client.controller.js
+++ public/modules/advertiser/controllers/siteTargeting.client.controller.js
@@ -2,16 +2,14 @@
 'use strict';
 
 angular.module('advertiser').controller('SiteTargetingController',
-    ['$scope','$stateParams','getSitesInCliqueBranch','Campaign','$TreeDnDConvert',
-        function($scope, $stateParams, getSitesInCliqueBranch, Campaign,$TreeDnDConvert){
+    ['$scope','$stateParams','getSitesInCliqueBranch','Campaign','flattenSiteCliques',
+        function($scope, $stateParams, getSitesInCliqueBranch, Campaign,flattenSiteCliques){
             Campaign.fromStateParams($stateParams, function(err, advertiser, campaign){
                 $scope.advertiser = advertiser;
                 $scope.campaign = campaign;
 
                 getSitesInCliqueBranch($scope.campaign.clique).then(function(response){
-                    $scope.sites = JSON.stringify(response.data, null, 2);
-
-                    console.log($scope.sites);
+                    $scope.sites = flattenSiteCliques(response.data);
                 });
 
                 // Begin Tree DND controller objects & functions
@@ -34,7 +32,8 @@ angular.module('advertiser').controller('SiteTargetingController',
                 $scope.col_defs = [
                     {
                         field: "Description"
-                    }, {
+                    },
+                    {
                         field:        "Description",
                         titleStyle:   {
                             'width': '80pt'
@@ -43,7 +42,8 @@ angular.module('advertiser').controller('SiteTargetingController',
                         cellClass:    'v-middle text-center',
                         displayName:  'Description',
                         cellTemplate: "<i class=\"fa {{ !node.Description ? 'fa-times text-danger-lter' : 'fa-check text-success' }} text\"></i>"
-                    }, {
+                    },
+                    {
                         displayName:  'Function',
                         cellTemplate: '<button ng-click="tree.addFunction(node)" class="btn btn-default btn-sm">Added Controller!</button>'
                     }, {
--- public/modules/publisher/services/publishers.client.service.js
+++ public/modules/publisher/services/publishers.client.service.js
@@ -10,21 +10,56 @@ angular.module('publisher').factory('Publisher', ['$resource',
 		    }
         );
 	}
-]);
-
-
-/**
- * Gets sites in specific Clique and formats for IVH-treeview plugin, adding
- * additional params used by placement targeting directive (logo_url, objectType)
- */
-angular.module('publisher').factory('getSitesInCliqueTree', ['$http', function($http){
+])
+.factory('getSitesInCliqueTree', ['$http', function($http){
+    /**
+     * Gets sites in specific Clique and formats for IVH-treeview plugin, adding
+     * additional params used by placement targeting directive (logo_url, objectType)
+     */
     return function(clique_id){
         return $http.get('/sitesinclique/' + clique_id,{})
     }
-}]);
-
-angular.module('publisher').factory('getSitesInCliqueBranch', ['$http', function($http){
+}])
+.factory('getSitesInCliqueBranch', ['$http', function($http){
+    /**
+     * Gets sites in specific Clique and formats for IVH-treeview plugin, adding
+     * additional params used by placement targeting directive (logo_url, objectType)
+     */
     return function(clique_id){
         return $http.get('/sitesincliquebranch/' + clique_id,{})
     }
+}])
+.factory('flattenSiteCliques',[function(){
+    /**
+     * Flattens tree structure returned from 'sitesincliquebranch' API endpoint
+     * to resemble closure-like tree structure required by Angular Tree DND library
+     */
+    return function(sitesInCliqueBranch){
+        var flattened = [];
+        sitesInCliqueBranch.forEach(function(clique){
+            var c = _.clone(clique);
+            delete c.sites;
+            c.parentId = null;
+            flattened.push(c);
+            clique.sites.forEach(function(site){
+                var s = _.clone(site);
+                delete s.pages;
+                s.parentId = clique._id;
+                flattened.push(s);
+                site.pages.forEach(function(page){
+                    var p = _.clone(page);
+                    p.parentId = site._id;
+                    flattened.push(p);
+                    page.placements.forEach(function(placement){
+                        var pl = _.clone(placement);
+                        pl.parentId = page._id;
+                        flattened.push(pl);
+                    });
+                });
+            });
+        });
+        return flattened;
+    };
 }]);
+
+
