f191b353b4fadedf8974fa49d7192e222c480df2
Ben Liang <bliang@cliquesads.com>
2016-01-14 18:34:18 -0500

    NF: Form pretty much done now.

--- public/modules/clique/controllers/cliques.client.controller.js
+++ public/modules/clique/controllers/cliques.client.controller.js
@@ -18,13 +18,39 @@ angular.module('clique').controller('CliqueController', ['$scope', '$stateParams
             });
 		};
 
+        // Row template to pass to multi-select
         $scope.rowTemplate = '<div class="col-md-3"><img ng-src="{{ option.logo_secure_url }}" class="client-logo-sm"/>' +
                                 '</div><div class="col-md-9"><h4 class="list-group-item-heading" data-ng-bind="option.name">' +
                                 '</h4><p class="list-group-item-text"> Active Campaigns: {{ option.campaigns.length }}</p></div>';
 
+        // Dialog box to select default advertisers
+        $scope.editDefaultAdvertisers = function(){
+            $scope.editCliqueForm.$dirty = true;
+            ngDialog.open({
+                template: '<strong>Select the Default Advertisers for this Clique</strong>' +
+                        '<div style="overflow: auto; height: 400px">' +
+                        '<row-multi-select base-model="ngDialogData.clique.default_advertisers" row-template="{{ ngDialogData.rowTemplate }}" options="ngDialogData.advertisers">' +
+                        '</row-multi-select></div>',
+                plain: true,
+                controller: ['$scope', function ($scope) {}],
+                data: {rowTemplate: $scope.rowTemplate, clique: $scope.clique, advertisers: $scope.advertisers}
+            });
+        };
+
+        // TODO: This doesn't work because child scope is entered when dialog opens
+        // TODO: just going to set dirty to true whenever dialog box is opened for now
+        // Default advertisers selector isn't part of the form, so have to manually set form to dirty
+        // when default advertisers is modified
+        //$scope.$watch(function(scope){
+        //    if (scope.clique) return scope.clique.default_advertisers
+        //}, function(newVal, oldVal){
+        //    if (newVal && oldVal && (newVal != oldVal)){
+        //        $scope.editCliqueForm.$dirty = true;
+        //    }
+        //});
+
         $scope.clique = null;
         $scope.advertisers = Advertiser.query();
-        $scope.editDefaultAdvertisers = false;
 
         $scope.resetEditForm = function(){
             $scope.editDefaultAdvertisers = false;
