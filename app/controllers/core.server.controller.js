45f2eec0ba8566a54bba6f6cf822f88207a18416
Ben Liang <bliang@cliquesads.com>
2016-01-24 15:21:35 -0500

    NF: Add reporting asOf to footer & restructure footer a little

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -1,12 +1,22 @@
 /* jshint node: true */ 'use strict';
 
+
+
 /**
  * Module dependencies.
  */
-exports.index = function(req, res) {
-	res.render('index', {
-		user: req.user || null,
-		request: req,
-        consoleVersion: res._headers['console-version']
-	});
+module.exports = function(db){
+    var getLatestHour = require('./aggregations.server.controller')(db).hourlyAdStat.getLatestHour;
+    return {
+        index: function(req, res) {
+            getLatestHour(function(err, result){
+                res.render('index', {
+                    user: req.user || null,
+                    request: req,
+                    latestHour: result.toUTCString(),
+                    consoleVersion: res._headers['console-version']
+                });
+            });
+        }
+    };
 };
