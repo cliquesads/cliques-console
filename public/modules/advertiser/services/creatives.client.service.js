0a66ff81426bfdee1433aabccfc91aef48cee0e8
Ben Liang <bliang@cliquesads.com>
2016-05-28 14:06:57 -0400

    NF: Add front-end changes to activate/deactivate creatives

---
+++ public/modules/advertiser/services/creatives.client.service.js
@@ -0,0 +1,24 @@
+/**
+ * Created by bliang on 5/28/16.
+ */
+angular.module('advertiser').factory('CreativeActivator',['$http',
+        function($http){
+            var activator = {};
+            function basePath(params){
+                return '/advertiser/' + params.advertiserId
+                    + '/campaign/' + params.campaignId
+                    + '/creativegroup/' + params.creativeGroupId
+                    + '/creative/' + params.creativeId;
+            }
+            activator.activate = function(params){
+                var path = basePath(params) + '/activate';
+                return $http.put(path);
+            };
+            activator.deactivate = function(params){
+                var path = basePath(params) + '/deactivate';
+                return $http.put(path);
+            };
+            return activator;
+        }
+    ]
+);
