c5d4ed6eb59e237eb9693396141d5a1cee77a6e3
Ben Liang <bliang@cliquesads.com>
2016-06-03 15:45:56 -0400

    BF: change Admin client side permissions

--- public/modules/admin/config/admin.client.config.js
+++ public/modules/admin/config/admin.client.config.js
@@ -3,7 +3,7 @@
 angular.module('admin').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-        Menus.addMenuItem('sidebar', 'Admin', 'admin', null, 'app.admin.main', false, ['admin'], 7, 'icon-settings');
+        Menus.addMenuItem('sidebar', 'Admin', 'admin', null, 'app.admin.main', false, ['networkAdmin'], 7, 'icon-settings');
         Menus.addSubMenuItem('sidebar', 'admin', 'Network Report', 'admin/network-report');
 	}
 ]);
