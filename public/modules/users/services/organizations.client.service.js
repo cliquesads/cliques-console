956b760177b7e78b8c9568783841a817b44d7bad
Ben Liang <bliang@cliquesads.com>
2016-06-09 10:15:54 -0400

    BF: forgot to change client service

--- public/modules/users/services/organizations.client.service.js
+++ public/modules/users/services/organizations.client.service.js
@@ -8,6 +8,7 @@ angular.module('users').factory('Organizations', ['$resource',
         return $resource('console/organization/:organizationId', { organizationId: '@_id'},
             {
                 update: { method: 'PATCH'},
+                get: { url: '/organization' },
                 create: { method: 'POST', url: '/organization'}
             }
         );
