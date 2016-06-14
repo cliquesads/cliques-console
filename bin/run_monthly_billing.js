1da5b441ae62b1b0d99c4a89bbe90be982ea0220
Ben Liang <bliang@cliquesads.com>
2016-06-14 01:19:30 -0400

    NF: add middle step to partition results by insertionOrder / non-insertionOrder dates prior to passing to payments step

--- bin/run_monthly_billing.js
+++ bin/run_monthly_billing.js
@@ -42,39 +42,54 @@ var publisherModels = new models.PublisherModels(db);
 var billing = require('../app/models/billing.server.model');
 var user = require('../app/models/user.server.model');
 
-var getBillingQuery = function(startDate, endDate, group){
-    return aggregationModels.HourlyAdStat
-        .aggregate([
-            { $match: {
-                hour: {
-                    $gte: startDate,
-                    $lt: endDate
-                }
-            }},
-            { $group: {
-                _id: group,
-                bids: {$sum: "$bids"},
-                imps: {$sum: "$imps"},
-                defaults: {$sum: "$defaults"},
-                spend: {$sum: "$spend"},
-                clicks: {$sum: "$clicks"},
-                view_convs: {$sum: "$view_convs"},
-                click_convs: {$sum: "$click_convs"}
-            }}
-        ]);
+
+/**
+ * Returns query object used for billing.  Must specify start & end dates, group & match are optional.
+ *
+ * @param startDate
+ * @param endDate
+ * @param group
+ * @param match
+ * @returns {Aggregate|Promise}
+ */
+var getBillingQuery = function(startDate, endDate, group, match){
+    var pipeline = [
+        { $match: {
+            hour: {
+                $gte: startDate,
+                $lt: endDate
+            }
+        }},
+        { $group: {
+            _id: group,
+            bids: {$sum: "$bids"},
+            imps: {$sum: "$imps"},
+            defaults: {$sum: "$defaults"},
+            spend: {$sum: "$spend"},
+            clicks: {$sum: "$clicks"},
+            view_convs: {$sum: "$view_convs"},
+            click_convs: {$sum: "$click_convs"}
+        }}
+    ];
+
+    if (match){
+        pipeline[0]["$match"] = _.merge(pipeline[0]["$match"], match);
+    }
+
+    return aggregationModels.HourlyAdStat.aggregate(pipeline);
 };
 
+// run billing in UTC
+var START_DATE = moment().tz('UTC').subtract(1, 'month').startOf('day').startOf('month').toDate();
+var END_DATE = moment().tz('UTC').startOf('day').startOf('month').toDate();
 
 /**
  * Step 1
  */
 var getAggregatesData = new Promise(function(resolve, reject){
-    // run billing in UTC
-    var startDate = moment().tz('UTC').subtract(1, 'month').startOf('day').startOf('month').toDate();
-    var endDate = moment().tz('UTC').startOf('day').startOf('month').toDate();
 
-    var advQuery = getBillingQuery(startDate, endDate, { advertiser: '$advertiser' });
-    var pubQuery = getBillingQuery(startDate, endDate, { publisher: '$publisher' });
+    var advQuery = getBillingQuery(START_DATE, END_DATE, { advertiser: '$advertiser' });
+    var pubQuery = getBillingQuery(START_DATE, END_DATE, { publisher: '$publisher' });
 
     async.parallel([
         function(callback){
@@ -124,31 +139,206 @@ var getAggregatesData = new Promise(function(resolve, reject){
  *
  * @param results
  */
-function populateOrganizations(results) {
-    return new Promise(function(resolve, reject){
-        // inner function does actual query for one resultset
-        function _inner(results, model, callback){
-            var orgs = [];
-            results.forEach(function(row){
-                if (row._id[model]){
-                    orgs.push(row._id[model].organization);
-                }
-            });
-            user.Organization.find({ _id: { $in: orgs}}, function(err, organizations){
-                if (err) return callback(err);
-                organizations.forEach(function(org) {
-                    var row = _.find(results, function(r){
-                        if (r._id[model]){
-                            return r._id[model].organization.toString() === org._id.toString();
-                        }
-                    });
-                    if (row){
-                        row.organization = org;
+var populateOrganizations = function(results) {
+    // inner function does actual query for one resultset
+    function _inner(results, model, callback){
+        var orgs = [];
+        results.forEach(function(row){
+            if (row._id[model]){
+                orgs.push(row._id[model].organization);
+            }
+        });
+        user.Organization.find({ _id: { $in: orgs}}, function(err, organizations){
+            if (err) return callback(err);
+            organizations.forEach(function(org) {
+                var row = _.find(results, function(r){
+                    if (r._id[model]){
+                        return r._id[model].organization.toString() === org._id.toString();
                     }
                 });
-                return callback(null, results);
+                if (row){
+                    row.organization = org;
+                }
             });
-        }
+            return callback(null, results);
+        });
+    }
+    return new Promise(function(resolve, reject){
+        // call in parallel for both advertiser & publisher data
+        async.parallel([
+            function(callback){ return _inner(results[0], 'advertiser', callback)},
+            function(callback){ return _inner(results[1], 'publisher', callback)}
+        ], function(err, populated){
+            if (err) return reject(err);
+            return resolve(populated);
+        });
+    });
+};
+
+/**
+ * Gets insertionOrders, if any, for this time period.
+ *
+ * Then, groups both advertiser & publisher query results by
+ * Organization (in case there are multiple advertisers/publishers
+ * for a given org).
+ *
+ * Finally, if there are any IO's for a given organization, re-queries HourlyAdStat
+ * to get partitioned dataset for billing purposes.
+ *
+ * @param orgPopulatedQueryResults
+ */
+var groupByOrgAndInsertionOrder = function(orgPopulatedQueryResults){
+    var initialAdvertiserResults = _.groupBy(orgPopulatedQueryResults[0], 'organization._id');
+    var initialPublisherResults = _.groupBy(orgPopulatedQueryResults[1], 'organization._id');
+    var advertiserResults = {};
+
+
+    // inner promise for InsertionOrders query
+    var getInsertionOrders = new Promise(function(resolve, reject) {
+        // find insertion orders with dates in this time period
+        billing.InsertionOrder.find({
+            end_date: {$gte: START_DATE},
+            start_date: {$lte: END_DATE}
+        }).exec(function (err, insertionOrders) {
+            if (err) return reject(err);
+            resolve(insertionOrders);
+        });
+    });
+
+    var getInsertionOrderData = function(insertionOrders){
+        return new Promise(function(resolve, reject){
+            // NOTE: Assumes only advertiser results need this IO partitioning.
+            // Don't have a use case for publishers needing this currently, nor do I
+            // see one in the immediate future.
+            // ALSO assumes that there's only one insertionorder per organization/dateRange.
+            // This is validated in pre-save method on InsertionOrder schema
+            async.forEachOf(initialAdvertiserResults, function(results, org, callback){
+                var insertionOrder = _.find(insertionOrders, function(io){
+                    return io.organization.toString() === org.toString()
+                });
+                if (insertionOrder && insertionOrder.length > 0){
+                    // see note above about insertionOrder uniqueness per org/dateRange
+                    insertionOrder = insertionOrder[0];
+                    // Do date range manipulation to get distinct query results for dates within IO & dates
+                    // outside IO.
+                    var insertionOrderRange = moment.range(insertionOrder.start_date, insertionOrder.end_date);
+                    var paymentRange = moment.range(START_DATE, END_DATE);
+                    // range for which payment will reflect IO terms
+                    var intersect = paymentRange.intersect(insertionOrderRange);
+                    // range (or ranges, if intersect is a subset of payment range)
+                    // for which payment will reflect default terms
+                    var complement = paymentRange.subtract(paymentRange);
+                    // get results & advertisers to re-query
+                    var thisOrgResults = advertiserResults[org];
+                    var advertisers = thisOrgResults.map(function(result){ return result._id.advertiser; });
+                    // get match query arg
+                    var advertisersMatch = { advertiser: { $in: advertisers }};
+                    var advertisersGroup = { advertiser: '$advertiser' };
+                    // create query for insertionOrder timeperiod
+                    var insertionOrderQueryFunc = function(callback){
+                        var query = getBillingQuery(intersect.start, intersect.end,
+                            advertisersMatch, advertisersGroup);
+                        query.exec(function(err, results){
+                            if (err) return callback(err);
+                            return callback(null,results);
+                        });
+                    };
+                    // may be multiple ranges in complement if IO range is subset of payments range
+                    if (complement.length > 0){
+                        var complementQueryFuncs = complement.map(function(range){
+                            return function(callback){
+                                var query = getBillingQuery(range.start, range.end,
+                                    advertisersMatch, advertisersGroup);
+                                query.exec(function(err, results){
+                                    if (err) return callback(err);
+                                    return callback(null,results);
+                                });
+                            }
+                        });
+                    }
+                    // put insertionOrderQuery first
+                    complementQueryFuncs.unshift(insertionOrderQueryFunc);
+                    // finally, execute queries in parallel
+                    async.parallel(complementQueryFuncs,function(err, allResults){
+                        if (err) return callback(err);
+                        // add insertionOrder to first results
+                        allResults[0].forEach(function(row){ row.insertionOrder = insertionOrder; });
+                        advertiserResults[org] = _.flatten(allResults);
+                        return callback();
+                    });
+                } else {
+                    // if no insertionOrder is found, just pass straight through to final results
+                    advertiserResults[org] = results;
+                    return callback();
+                }
+            }, function(err){
+                if (err) return reject(err);
+                return resolve(advertiserResults);
+            });
+        });
+    };
+
+    // wrapper promise that executes both insertionOrder & insertionOrderData steps
+    return new Promise(function(resolve, reject) {
+        getInsertionOrders
+            .then(getInsertionOrderData, function (err) {
+                return reject(err);
+            })
+            .then(function(advertiserResults){
+                return resolve([advertiserResults, initialPublisherResults]);
+            }, function(err){ return reject(err); });
+    });
+};
+
+var createPayments = function(populatedResults){
+    function _inner(results, model, callback){
+        results.forEach(function(row){
+            if (row._id[model]){
+
+                // ######## CREATE NEW PAYMENT ###########
+
+                var payment = new billing.Payment({
+                    start_date: START_DATE,
+                    end_date: END_DATE,
+                    organization: row.organization._id,
+                    paymentType: model,
+                    imps: row.imps,
+                    spend: row.spend,
+                    clicks: row.clicks,
+                    view_convs: row.view_convs,
+                    click_convs: row.click_convs,
+                    billingMethod: row.organization.billingPreference,
+                    status: "Pending"
+                });
+
+                // ######### BEGIN FEE LOGIC #############
+
+                var fees = row.organization.fees.filter(function(fee){
+                    return fee.type === model;
+                });
+                // TODO: Need to add validation to Organization to prevent multiple
+                // TODO: fees of same type
+                if (fees.length > 1){
+                    return callback("ERROR: Multiple fees of type: " + model + " found for" +
+                        " organization " + row.organization.name);
+                } else {
+                    payment.fee = fees[0];
+                }
+
+                // ########## BEGIN INSERTION ORDER LOGIC ##########
+
+                // ########## BEGIN CONTRACT TYPE LOGIC ###########
+
+
+                // ########## CALCULATE TOTALS ############
+
+            }
+
+        });
+
+    }
+
+    return new Promise(function(resolve, reject){
 
         // call in parallel for both advertiser & publisher data
         async.parallel([
@@ -159,7 +349,7 @@ function populateOrganizations(results) {
             return resolve(populated);
         });
     });
-}
+};
 
 
 mongoose.connect(exchangeMongoURI, exchangeMongoOptions, function(err, logstring) {
@@ -176,7 +366,11 @@ mongoose.connect(exchangeMongoURI, exchangeMongoOptions, function(err, logstring
             function (err) { console.error(err); }
         )
         .then(
-            function(populated){ console.log(populated); },
+            function(populated){ groupByOrgAndInsertionOrder(populated); },
             function(err){ console.error(err); }
+        )
+        .then(
+            function(populated){ createPayments(populated); },
+            function(err){ console.error(err);}
         );
 });
