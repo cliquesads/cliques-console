b5158212b538188e45df1a795613dd87267baa13
Ben Liang <bliang@cliquesads.com>
2016-06-21 00:33:42 -0400

    NF: add billing section back to profile and fix grid issue

--- public/modules/users/controllers/billing.client.controller.js
+++ public/modules/users/controllers/billing.client.controller.js
@@ -1,15 +1,20 @@
-angular.module('users').controller('BillingController', ['$scope', '$http', '$location', 'Users', 'Authentication',
+angular.module('users').controller('BillingController', ['$scope', '$http', '$location', 'Users', 'Authentication','Notify',
     'Organizations',
-    function($scope, $http, $location, Users, Authentication, Organizations) {
+    function($scope, $http, $location, Users, Authentication,Notify, Organizations) {
         $scope.user = Authentication.user;
         $scope.organization = new Organizations(Authentication.user.organization);
-
         $scope.handleStripe = function(status, response){
             if(response.error) {
-                // there was an error. Fix it.
+                Notify.alert('Stripe encountered the following error: ' + response.error, {status: 'danger'});
             } else {
                 // got stripe token, now charge it or smt
-                token = response.id
+                $scope.organization.stripeToken = response.id;
+                $scope.organization.$update(function(response){
+                    $scope.organization = response;
+                    Notify.alert('Organization Saved Successfully', {status: 'success'});
+                }, function(response){
+                    Notify.alert(response.data.message, {status: 'danger'});
+                });
             }
         }
     }
