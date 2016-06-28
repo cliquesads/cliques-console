08be1699c7ed76396317bdc2da4be56b3b39b619
Ben Liang <bliang@cliquesads.com>
2016-06-28 17:08:42 -0400

    NF: Fixed CC add form

--- public/modules/users/controllers/billing.client.controller.js
+++ public/modules/users/controllers/billing.client.controller.js
@@ -3,30 +3,55 @@ angular.module('users').controller('BillingController', ['$scope', '$http', '$lo
     function($scope, $http, $location, Users, Authentication,Notify, Organizations, Payment) {
         $scope.user = Authentication.user;
         $scope.organization = new Organizations(Authentication.user.organization);
+
+        // get all payments to populate billing history
         $scope.payments = Payment.query();
+
+        // control variable for CC add form toggling
         $scope.editStripe = false;
 
-        $scope.organization.$getStripeCustomer().then(function(customer){
-            $scope.stripeCustomer = customer;
-            $scope.defaultCard = customer.sources.data.filter(function(source){
-                return source.id === customer.default_source
-            })[0];
-        }, function(response){
-            console.error(response.data.message);
-        });
+        // loading control var
+        $scope.loading = false;
 
-        $scope.handleStripe = function(status, response){
+        // Load stripe Customer object associated w/ Organization, if applicable
+        $scope.stripeCustomer = null;
+        var getStripeCustomer = function(){
+            if ($scope.organization.stripeCustomerId){
+                var org = new Organizations($scope.organization);
+                org.$getStripeCustomer().then(function(customer){
+                    $scope.stripeCustomer = customer;
+                    $scope.defaultCard = customer.sources.data.filter(function(source){
+                        return source.id === customer.default_source
+                    })[0];
+                }, function(response){
+                    console.error(response.data.message);
+                });
+            }
+        };
+        getStripeCustomer();
+
+        $scope.addToken = function(status, response){
+            $scope.loading = true;
             if(response.error) {
                 Notify.alert('Stripe encountered the following error: ' + response.error, {status: 'danger'});
             } else {
-                // got stripe token, now charge it or smt
-                $scope.organization.$saveStripeToken({ stripeToken: response.id })
-                    .then(function(response){
-                        $scope.organization = response;
-                        Notify.alert('Your credit card has been saved, thanks!', {status: 'success'});
-                    }, function(response){
-                        Notify.alert(response.data.message, {status: 'danger'});
-                    });
+                // first update org to save billing preference
+                $scope.organization.$update().then(function(org){
+                    // got stripe token, now charge it or smt
+                    return $scope.organization.$saveStripeToken({ stripeToken: response.id });
+                }).then(function(response){
+                    // now handle post-save steps
+                    $scope.organization = response;
+                    $scope.loading = false;
+                    Notify.alert('Your credit card has been saved, thanks!', {status: 'success'});
+                    // update default card setting
+                    getStripeCustomer();
+                    // close form
+                    $scope.editStripe = false;
+                }, function(response){
+                    $scope.loading = false;
+                    Notify.alert(response.data.message, {status: 'danger'});
+                });
             }
         }
     }
