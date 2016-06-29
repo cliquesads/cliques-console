909a0698fe02649092f57105140ee4c2cbe68447
Ben Liang <bliang@cliquesads.com>
2016-06-28 22:32:59 -0400

    BF: Fix logic behind master save & cancel buttons

--- public/modules/users/controllers/billing.client.controller.js
+++ public/modules/users/controllers/billing.client.controller.js
@@ -2,18 +2,19 @@ angular.module('users').controller('BillingController', ['$scope', '$http', '$lo
     'Organizations', 'Payment',
     function($scope, $http, $location, Users, Authentication,Notify, Organizations, Payment) {
         $scope.user = Authentication.user;
-        $scope.organization = new Organizations(Authentication.user.organization);
+        var organization = $scope.organization = new Organizations(Authentication.user.organization);
 
         // get all payments to populate billing history
         $scope.payments = Payment.query();
 
         // control variable for CC add form toggling
         $scope.editStripe = false;
-
-        // loading control var
+        // Stripe form "loading" glyph control var
         $scope.loading = false;
 
-        // Load stripe Customer object associated w/ Organization, if applicable
+        /**
+         * Load stripe Customer object associated w/ Organization, if applicable
+         */
         $scope.stripeCustomer = null;
         var getStripeCustomer = function(){
             if ($scope.organization.stripeCustomerId){
@@ -28,8 +29,26 @@ angular.module('users').controller('BillingController', ['$scope', '$http', '$lo
                 });
             }
         };
+        // get stripe customer on load
         getStripeCustomer();
 
+
+        /**
+         * Controls for master Save & Cancel buttons
+         */
+        // capture initial billing preference to know when to update organization
+        $scope.initialBillingPreference = organization.billingPreference;
+        // whether or not to show save button bar
+        $scope.allowSave = false;
+
+        $scope.reset = function(){
+            $scope.organization = Organizations.get({
+                organizationId: Authentication.user.organization._id
+            });
+            $scope.allowSave = false;
+        };
+
+        // Update billing preference for organization.  Called on master "save"
         $scope.updateOrganization = function(){
             $scope.organization.$update(function(response){
                 $scope.organization = response;
@@ -39,23 +58,19 @@ angular.module('users').controller('BillingController', ['$scope', '$http', '$lo
                 Notify.alert(response.data.message, {status: 'danger'});
             });
         };
-
-        $scope.reset = function(){
-            $scope.organization = Organizations.get({
-                organizationId: Authentication.user.organization._id
-            });
-            $scope.allowSave = false;
-        };
-
-        $scope.allowSave = false;
+        // Only show save & cancel buttons when billingPreference has changed & is not Stripe,
+        // which has its own Save function.
         $scope.$watch('organization.billingPreference', function(newValue, oldValue){
-            if (oldValue && oldValue != newValue){
-                if (newValue == 'Check'){
-                    $scope.allowSave = true;
-                }
-            }
+            $scope.allowSave = (newValue != $scope.initialBillingPreference
+                && newValue != "Stripe");
         });
 
+
+        /**
+         * Handler for Stripe new card form.
+         * Gets called by angular-payments directive after it calls Stripe to get token, so
+         * response.id = token.
+         */
         $scope.addToken = function(status, response){
             $scope.loading = true;
             if(response.error) {
@@ -70,7 +85,7 @@ angular.module('users').controller('BillingController', ['$scope', '$http', '$lo
                     // now handle post-save steps
                     $scope.organization = response;
                     $scope.loading = false;
-                    Notify.alert('Your credit card has been saved, thanks!', {status: 'success'});
+                    Notify.alert('Your credit card has been saved, thanks! When you run a campaign, this card will be billed automatically.', {status: 'success'});
                     // update default card setting
                     getStripeCustomer();
                     // close form
