51b7f193920511e955216760d50fb12dd6ca12c3
Ben Liang <bliang@cliquesads.com>
2016-06-29 19:05:21 -0400

    NF: Bank account number & routing number validation

--- public/modules/payments/directives/stripe-bank-account-form.js
+++ public/modules/payments/directives/stripe-bank-account-form.js
@@ -1,7 +1,43 @@
-/**
- * Created by bliang on 1/11/16.
- */
-angular.module('users').directive('stripeBankAccountForm', function(){
+
+angular.module('payments').directive('accountnumber', function(){
+    return {
+        require: 'ngModel',
+        scope: {
+            country: '@'
+        },
+        link: function(scope, elem, attrs, ctrl){
+            scope.$watch("country", function(newVal, oldVal){
+                scope.country = newVal;
+            });
+            ctrl.$validators.accountnumber = function(modelValue, viewValue){
+                if (ctrl.$isEmpty(modelValue)){
+                    return true;
+                }
+                return Stripe.bankAccount.validateAccountNumber(modelValue, scope.country);
+            }
+        }
+    }
+})
+.directive('routingnumber', function(){
+    return {
+        require: 'ngModel',
+        scope: {
+            country: '@'
+        },
+        link: function(scope, elem, attrs, ctrl){
+            scope.$watch("country", function(newVal, oldVal){
+                scope.country = newVal;
+            });
+            ctrl.$validators.routingnumber = function(modelValue, viewValue){
+                if (ctrl.$isEmpty(modelValue)){
+                    return true;
+                }
+                return Stripe.bankAccount.validateRoutingNumber(modelValue, scope.country);
+            }
+        }
+    }
+})
+.directive('stripeBankAccountForm', function(){
     'use strict';
     return {
         restrict: 'E',
@@ -19,11 +55,18 @@ angular.module('users').directive('stripeBankAccountForm', function(){
                 account_holder_type: 'company'
             };
 
+            scope.submitted = false;
+            scope.validateInput = function(name, type) {
+                var input = scope.stripeBankAccountForm[name];
+                return (input.$dirty || scope.submitted) && input.$error[type];
+            };
+
             scope.wrapper = function(status, response){
                 scope.onSave({ status: status, response: response });
             };
 
             scope.submit = function(){
+                scope.submitted = true;
                 Stripe.bankAccount.createToken(scope.account, scope.wrapper);
             };
         }
