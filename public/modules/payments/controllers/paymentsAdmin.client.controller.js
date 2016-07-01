22703e9808b8e9d15e01255e672731b09cba15e8
Ben Liang <bliang@cliquesads.com>
2016-06-30 20:38:24 -0400

    NF: Add files for payment admin, and move payment service to payments module

---
+++ public/modules/payments/controllers/paymentsAdmin.client.controller.js
@@ -0,0 +1,6 @@
+angular.module('payments').controller('PaymentAdminController', ['$scope', '$http', '$location', 'Users',
+    'Authentication','Notify','Organizations', 'Payment','ngDialog',
+    function($scope, $http, $location, Users, Authentication,Notify, Organizations, Payment, ngDialog) {
+
+    }
+]);
