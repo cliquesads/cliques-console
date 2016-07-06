a86e822a402c7e443c28d3f4453898493eadee7e
Ben Liang <bliang@cliquesads.com>
2016-07-06 14:26:13 -0400

    BF: Rev-shares should be positive, not negative

--- app/models/billing.server.model.js
+++ app/models/billing.server.model.js
@@ -340,6 +340,11 @@ PaymentSchema.methods.calculateFeeOrRevShareLineItem = function(){
             feeLineItem.lineItemType = "Fee";
         } else {
             feeLineItem.lineItemType = "RevShare";
+            // flip the sign on rev-shares.  Revenue is represented as negative,
+            // so feeLineItem.amount will be negative as well.  This would mean that
+            // when totalAmount sums all LI's, revShare will increase abs value
+            // of the totalAmount, when it should do the opposite.
+            feeLineItem.amount = feeLineItem.amount * -1;
         }
         // delegate description to static method
         feeLineItem = Payment.lineItem_generateDescription(feeLineItem);
