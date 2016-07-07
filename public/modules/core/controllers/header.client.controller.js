c544fad4dc8df4a3c06a81d6aa1710aaac3e2556
Ben Liang <bliang@cliquesads.com>
2016-07-07 17:58:01 -0400

    NF: Add notifications for billing center

--- public/modules/core/controllers/header.client.controller.js
+++ public/modules/core/controllers/header.client.controller.js
@@ -1,10 +1,23 @@
 'use strict';
 
-angular.module('core').controller('HeaderController', ['$scope', 'Authentication', 'Menus',
-	function($scope, Authentication, Menus) {
+angular.module('core').controller('HeaderController', ['$scope', 'Authentication', 'Organizations', 'Menus',
+	function($scope, Authentication, Organizations, Menus) {
 		$scope.authentication = Authentication;
 		$scope.isCollapsed = false;
 		$scope.menu = Menus.getMenu('topbar');
+
+		if (user) {
+			$scope.organization = Organizations.get({
+				organizationId: Authentication.user.organization._id
+			});
+			$scope.organization.$promise.then(function(org){
+				if (org.payments){
+					$scope.payments = org.payments.filter(function(p){
+						return (p.status === 'Pending' || p.status === 'Overdue');
+					});
+				}
+			});
+		}
 
 		$scope.toggleCollapsibleMenu = function() {
 			$scope.isCollapsed = !$scope.isCollapsed;
