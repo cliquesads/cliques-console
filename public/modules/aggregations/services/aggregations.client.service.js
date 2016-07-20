8e81b5d2d21a8330b090027917c977d1dfc9bc2f
Ben Liang <bliang@cliquesads.com>
2016-07-20 00:42:14 -0500

    NF: toggle between aggregation models

--- public/modules/aggregations/services/aggregations.client.service.js
+++ public/modules/aggregations/services/aggregations.client.service.js
@@ -5,8 +5,11 @@
  *
  *
  */
-angular.module('aggregations').factory('HourlyAdStat', ['$http',
-	function($http) {
+angular.module('aggregations').factory('HourlyAdStat', ['Authentication','$http',
+	function(Authentication, $http) {
+        // TODO: HACK, don't want to add demo flag on orgs yet so just hardcode here temporarily
+        var demo = Authentication.user.organization._id === '578e90fced74a996159254a3';
+
         var base_path    = '/console/hourlyadstat';
         var adv_path     = base_path + '/adv';
         var pub_path     = base_path + '/pub';
@@ -27,21 +30,36 @@ angular.module('aggregations').factory('HourlyAdStat', ['$http',
                     }
                 });
                 var path = basepath + relative_path;
+                // set demo flag
+                queryParams = queryParams || {};
+                queryParams.demo = demo;
                 return $http.get(path, {params: queryParams});
             };
         }
         hourlyadstatfactory.query = function(queryParams){
+            // set demo flag
+            queryParams = queryParams || {};
+            queryParams.demo = demo;
             return $http.get(base_path, {params: queryParams});
         };
         hourlyadstatfactory.advSummaryQuery = function(queryParams){
+            // set demo flag
+            queryParams = queryParams || {};
+            queryParams.demo = demo;
             return $http.get(advSummary_path, {params: queryParams});
         };
         hourlyadstatfactory.pubSummaryQuery = function(queryParams){
+            // set demo flag
+            queryParams = queryParams || {};
+            queryParams.demo = demo;
             return $http.get(pubSummary_path, {params: queryParams});
         };
         hourlyadstatfactory.advQuery = constructQueryFunc(['advertiserId', 'campaignId', 'creativegroupId','creativeId'], adv_path);
         hourlyadstatfactory.pubQuery = constructQueryFunc(['publisherId', 'siteId', 'pageId','placementId'], pub_path);
         hourlyadstatfactory.cliqueQuery = function(queryParams){
+            // set demo flag
+            queryParams = queryParams || {};
+            queryParams.demo = demo;
             return $http.get(clique_path, {params: queryParams});
         };
 
