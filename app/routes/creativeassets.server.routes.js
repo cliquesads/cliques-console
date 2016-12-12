33dacf2c8f3cb87887e2171bfe5b57da77cd77fa
Ben Liang <bliang@cliquesads.com>
2016-12-11 22:01:56 -0500

    NF: Logo & creativeAsset API docs

--- app/routes/creativeassets.server.routes.js
+++ app/routes/creativeassets.server.routes.js
@@ -11,11 +11,19 @@ module.exports = function(db, routers){
          * @api {post} /creativeassets Upload Creative Asset
          * @apiName UploadCreativeAsset
          * @apiGroup CreativeAsset
-         * @apiDescription Uploads a Creative Asset (an image file that's served as an ad creative) to Google Cloud Storage,
-         *  as well as local filesystem.
+         * @apiVersion 0.1.0
+         * @apiPermission networkAdmin
+         * @apiPermission advertiser
+         * @apiPermission publisher
          *
+         * @apiDescription Uploads a single Creative Asset (an image file that's served as an ad creative) to Google Cloud Storage,
+         *  as well as local filesystem (currently stores files locally under public/uploads only for backup).
          *
+         * @apiParam (multipart/form-data) {File} CreativeAsset File object representing creative asset. No validation
+         *  is currently performed server-side on this file, so make sure it is a valid creative file before uploading.
          *
+         * @apiSuccess {Object} responseBody
+         * @apiSuccess {String} responseBody.url Google Cloud Storage URL of newly uploaded file.
          */
         .post(upload.single('file'), creativeassets.create);
 };
--- app/routes/logos.server.routes.js
+++ app/routes/logos.server.routes.js
@@ -7,5 +7,23 @@ module.exports = function(db, routers){
     var logos = require('../controllers/logos.server.controller')(db);
     var router = routers.apiRouter;
     router.route('/logos')
+        /**
+         * @api {post} /logos Upload Creative Asset
+         * @apiName UploadLogos
+         * @apiGroup Logos
+         * @apiVersion 0.1.0
+         * @apiPermission networkAdmin
+         * @apiPermission advertiser
+         * @apiPermission publisher
+         *
+         * @apiDescription Uploads a single logo (an image file that represents an Organization logo) to Google Cloud Storage,
+         *  as well as local filesystem (currently stores files locally under public/uploads only for backup).
+         *
+         * @apiParam (multipart/form-data) {File} Logo File object representing org logo. No validation
+         *  is currently performed server-side on this file, so make sure it is a valid logo before uploading.
+         *
+         * @apiSuccess {Object} responseBody
+         * @apiSuccess {String} responseBody.url Google Cloud Storage URL of newly uploaded file.
+         */
         .post(upload.single('file'), logos.create);
 };
