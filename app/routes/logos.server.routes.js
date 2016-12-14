ef3aaba08d81810773e5015c96751716fd276798
Ben Liang <bliang@cliquesads.com>
2016-12-14 17:30:57 -0500

    NF: Finish organization documentation

--- app/routes/logos.server.routes.js
+++ app/routes/logos.server.routes.js
@@ -8,9 +8,9 @@ module.exports = function(db, routers){
     var router = routers.apiRouter;
     router.route('/logos')
         /**
-         * @api {post} /logos Upload Creative Asset
-         * @apiName UploadLogos
-         * @apiGroup Logos
+         * @api {post} /logos Upload Organization Logo
+         * @apiName UploadLogo
+         * @apiGroup Logo
          * @apiVersion 0.1.0
          * @apiPermission networkAdmin
          * @apiPermission advertiser
@@ -18,6 +18,8 @@ module.exports = function(db, routers){
          *
          * @apiDescription Uploads a single logo (an image file that represents an Organization logo) to Google Cloud Storage,
          *  as well as local filesystem (currently stores files locally under public/uploads only for backup).
+         *
+         * TODO: This really belongs under the [Organization](#api-Organization) namespace instead of having its own.
          *
          * @apiParam (multipart/form-data) {File} Logo File object representing org logo. No validation
          *  is currently performed server-side on this file, so make sure it is a valid logo before uploading.
