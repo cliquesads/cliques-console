512890cb7a019e5cc1b4f2af2ebf19100dad8939
Chuoxian Yang <yangchuoxian.appdev@gmail.com>
2017-01-09 15:05:38 +0800

    Finished action beacon module, but not sure how to decide which advertiser when entering action beacon entry yet

--- public/modules/actionbeacon/controllers/actionbeacons.client.controller.js
+++ public/modules/actionbeacon/controllers/actionbeacons.client.controller.js
@@ -3,7 +3,110 @@
 
 angular.module('actionbeacon').controller('ActionBeaconController', [
     '$scope',
-    function($scope) {
-    	
+    '$location',
+    'ngDialog',
+    'Advertiser',
+    'ActionBeacon',
+    function($scope, $location, ngDialog, Advertiser, ActionBeacon) {
+        $scope.advertiser = {};
+        $scope.actionbeacon = {
+            name: null
+        };
+
+        $scope.update = function(callback) {
+            this.advertiser.$update(function() {
+                if (callback) {
+                    callback();
+                }
+            }, function(errorResponse) {
+                $scope.saveerror = errorResponse.data.message;
+            });
+        };
+
+        $scope.advertisers = Advertiser.query(function(advertisers) {
+            if (advertisers.length > 0) {
+                $scope.advertiser = $scope.advertisers[0];
+            }
+        });
+        $scope.showActionBeaconTagDialog = function(actionbeacon) {
+            ngDialog.open({
+                template: 'modules/actionbeacon/views/partials/get-actionbeacon-code.html',
+                controller: ['$scope', 'ActionBeacon', function($scope, ActionBeacon) {
+                    $scope.advertiser = $scope.ngDialogData.advertiser;
+                    $scope.actionbeacon = $scope.ngDialogData.actionbeacon;
+                    $scope.options = {
+                        secure: false
+                    };
+                    $scope.getTag = function() {
+                        ActionBeacon.getTag({
+                            advertiserId: $scope.advertiser._id,
+                            actionbeaconId: actionbeacon._id,
+                            secure: $scope.options.secure
+                        }).then(function(response) {
+                            $scope.tag = response.data.tag;
+                        });
+                    };
+
+                    $scope.getTag();
+
+                    $scope.toggleSecureOption = function() {
+                    	$scope.getTag();
+                    };
+                }],
+                data: {
+                    advertiser: $scope.advertiser,
+                    actionbeacon: actionbeacon
+                }
+            })
+        };
+        $scope.showDeleteActionBeaconDialog = function(actionbeacon) {
+            ngDialog.open({
+                template: 'modules/actionbeacon/views/partials/delete-actionbeacon.html',
+                controller: ['$scope', function($scope) {
+                    $scope.advertiser = $scope.ngDialogData.advertiser;
+                    $scope.update = function() {
+                        var advertiser = $scope.advertiser;
+                        advertiser.$update(function() {}, function(errorResponse) {
+                            $scope.error = errorResponse.data.message;
+                        });
+                    };
+                    $scope.confirm = function(val) {
+                        if (val === 1) {
+                            // first find indices of desired creative
+                            var ab_ind = _.findIndex($scope.advertiser.actionbeacons, function(ab) {
+                                return ab === actionbeacon;
+                            });
+                            // remove from actionbeacons document array
+                            $scope.advertiser.actionbeacons.splice(ab_ind, 1);
+                            $scope.update();
+                        }
+                    };
+                }],
+                data: { advertiser: $scope.advertiser }
+            });
+        };
+        $scope.showCreateNewActionBeaconDialog = function() {
+            ngDialog.open({
+                template: 'modules/actionbeacon/views/partials/new-actionbeacon.html',
+                controller: ['$scope', function($scope) {
+                    $scope.advertiser = $scope.ngDialogData.advertiser;
+                    $scope.submitNewActionBeacon = function(e, newActionBeaconName) {
+                        e.preventDefault();
+                        if (this.newActionBeacon.$valid) {
+                            this.advertiser.actionbeacons.push(this.actionbeacon);
+                            this.advertiser.$update(function() {
+                                $scope.closeThisDialog(0)
+                            }, function(errorResponse) {
+                                $scope.saveerror = errorResponse.data.message;
+                            });
+                        } else {
+                            $scope.closeThisDialog(0)
+                            return false;
+                        }
+                    };
+                }],
+                data: { advertiser: $scope.advertiser }
+            })
+        };
     }
 ]);
