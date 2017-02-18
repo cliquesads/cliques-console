0e5562f623ef569638b1a19a2513f4b78e2e7bd3
Ben Liang <bliang@cliquesads.com>
2017-02-18 11:49:24 -0700

    RF: Moved migration method into bin/migrations as an executable, ran it successfully

--- app/controllers/screenshots.server.controller.js
+++ app/controllers/screenshots.server.controller.js
@@ -174,47 +174,6 @@ module.exports = function(db) {
 				}
 			}
 			next();
-		},
-
-		// One-time script to get screenshot models all the missing ObjectIds.
-		completeModelIdsForScreenshots: function (req, res) {
-			var count = 0;
-			var parentAdvertiser, parentCampaign, parentPublisher, parentPage, parentSite;
-			var advertiserId, publisherId, pageId, siteId, campaignId, campaignName;
-			screenshotModels.Screenshot.find({}, function(err, screenshots) {
-				screenshots.forEach(function(screenshot, index) {
-
-					advertiserModels.getNestedObjectById('' + screenshot.creativegroup, 'CreativeGroup', function(err, crg) {
-						if (crg) {
-							parentAdvertiser = crg.parent_advertiser;
-							parentCampaign = crg.parent_campaign;
-						} else {
-							console.error('crg not exist for ' + screenshot.creativegroup);
-						}
-						publisherModels.getNestedObjectById('' + screenshot.placement, 'Placement', function(err, placement) {
-							if (placement) {
-								parentPublisher = placement.parent_publisher;
-								parentPage = placement.parent_page;
-								parentSite = placement.parent_site;
-							} else {
-								console.error('placement not exit for ' + screenshot.placement);
-							}
-
-							screenshot.advertiser = parentAdvertiser._id;
-							screenshot.publisher = parentPublisher._id;
-							screenshot.page = parentPage._id;
-							screenshot.site = parentSite._id;
-							screenshot.campaign = parentCampaign._id;
-
-							screenshot.save();
-							count ++;
-							if (count === screenshots.length) {
-								return res.send('done');
-							}
-						});
-					});
-				});
-			});
-		},
+		}
 	};
 };
