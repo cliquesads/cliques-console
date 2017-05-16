1440a612aee43392b666eb021815fb0436d1c14b
Ben Liang <bliang@cliquesads.com>
2017-05-16 02:44:04 -0400

    Directives for copy text areas, was getting ridiculous w/ all the validation

---
+++ public/modules/advertiser/directives/native-copy-long.js
@@ -0,0 +1,20 @@
+angular.module('advertiser').directive('nativeCopyLong', ['NATIVE_SPECS', function(NATIVE_SPECS) {
+    'use strict';
+    return {
+        restrict: 'E',
+        scope: {
+            ngModel: '=',
+            form: '=',
+            rows: '@',
+            wizardstep: '@',
+            index: '@'
+        },
+        templateUrl: 'modules/advertiser/views/partials/native-copy-long.html',
+        link: function(scope, element, attrs){
+            scope.NATIVE_SPECS = NATIVE_SPECS;
+            scope.rows = scope.rows || 6;
+            scope.inputId = scope.index + '_copyLong';
+            scope.copyLongModel = scope.ngModel.file ? scope.ngModel.copyLong : scope.ngModel.native.copyLong;
+        }
+    };
+}]);
