6d5c78ef751e37eb0f0b5b4bb39b944ec02ccf7d
Chuoxian Yang <yangchuoxian.appdev@gmail.com>
2017-06-05 18:56:02 +0800

    Geo target view basic structure

--- public/modules/advertiser/controllers/geoTargeting.client.controller.js
+++ public/modules/advertiser/controllers/geoTargeting.client.controller.js
@@ -2,8 +2,8 @@
 'use strict';
 
 angular.module('advertiser').controller('GeoTargetingController', [
-	'$scope', '$stateParams', 'Notify', 'campaign',
-	function($scope, $stateParams, Notify, campaign) {
+	'$scope', '$stateParams', 'Notify', 'campaign', 'ngDialog',
+	function($scope, $stateParams, Notify, campaign, ngDialog) {
 
 		/**
 		 * Get Campaign from URL state params on load
@@ -12,5 +12,15 @@ angular.module('advertiser').controller('GeoTargetingController', [
 		$scope.campaignIndex = campaign.index;
 		$scope.campaign = campaign.campaign;
 
+		$scope.getAllGeosHelp = function() {
+			ngDialog.open({
+				className: 'ngdialog-theme-default',
+				template: 'modules/advertiser/views/partials/all-geos-help-text.html',
+				controller: ['$scope', function($scope) {
+					$scope.campaign = $scope.ngDialogData.campaign;
+				}],
+				data: {campaign: $scope.campaign}
+			})
+		};
 	}
 ]);
