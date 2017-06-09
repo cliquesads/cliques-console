2e74c6bbe5bc4fae519de2308ba133eb2e65296f
Chuoxian Yang <yangchuoxian.appdev@gmail.com>
2017-06-09 20:43:27 +0800

    Partial implementation of tree format of GeoTree

--- public/modules/advertiser/controllers/geoTargeting.client.controller.js
+++ public/modules/advertiser/controllers/geoTargeting.client.controller.js
@@ -89,6 +89,9 @@ angular.module('advertiser').controller('GeoTargetingController', [
 			delete $rootScope.selectedGeo;
 			$state.reload();
 		};
+		//====================================================//
+		//================ END of Map Settings ================//
+		//====================================================//
 
 		$scope.getAllGeosHelp = function() {
 			ngDialog.open({
@@ -159,39 +162,6 @@ angular.module('advertiser').controller('GeoTargetingController', [
 		};
 
 		/**
-		 * Show all sub-geo within the clicked geo node,
-		 * if clicked geo node is country, show all its regions,
-		 * if clicked geo node is region, show all its cities
-		 */
-		$scope.expandGeoNode = function(node) {
-			if (node.nodeType === 'Country') {
-				Region.query({country: node.target}).$promise.
-				then(function(response) {
-					$scope.countryNode = node;
-					if (response.length >= 1) {
-						$scope.geoTargets = [];
-						response.forEach(function(region) {
-							var newRegionNode = initializeGeoNode(region.name, 'Region', region._id);
-							$scope.geoTargets.push(newRegionNode);
-						});
-					}
-				});
-			} else if (node.nodeType === 'Region') {
-				City.query({region: node.target}).$promise.
-				then(function(response) {
-					$scope.regionNode = node;
-					if (response.length > 1) {
-						$scope.geoTargets = [];
-						response.forEach(function(city) {
-							var newCityNode = initializeGeoNode(city.name, 'City', city._id);
-							$scope.geoTargets.push(newCityNode);
-						});
-					}
-				});
-			}
-		};
-
-		/**
 		 * Save handler. Converts geo_targets to geoTargetSchema DB format,
 		 * and converts blocked_geos to DB format, updates advertiser.
 		 */
@@ -226,6 +196,147 @@ angular.module('advertiser').controller('GeoTargetingController', [
 		// Undo all unsaved changes
 		$scope.reset = function() {
 			// TO-DO:::ycx
+		};
+
+		//====================================================//
+		//================ END of Map Settings ===============//
+		//====================================================//
+
+		/**
+		 * Adds custom methods & properties to tree node object.
+		 *
+		 * Wrapping in custom prototype won't work because site-tree-dnd relies
+		 * on certain ownProperties of node objects, so pushing these up the
+		 * prototype chain breaks it.  So this is sort of a hack to add methods
+		 * & properties directly to clones of raw node instances.
+		 *
+		 * @param node
+		 * @param nodeType
+		 * @param parentId
+		 * @returns {node}
+		 */
+		var _initializeGeoTreeNode = function(node, nodeType, parentId) {
+			// Create node clone
+			var newNode = _.clone(node);
+
+			// Add custom node properties
+			newNode.parentId = parentId; // Needed for conversion to Geo Treen DND format
+			newNode.nodeType = nodeType;
+			newNode.__hideSlider__ = false;
+			// Set initial state as overridden so that it only can be set false
+			// when slider is engaged by user
+			newNode.__overridden__ = true;
+			newNode.__lock__ = false;
+			newNode.weight = node.weight || 1.0;
+
+			// Properties used by blocked_geo settings
+			newNode.explicit = false;
+
+			// Clear old children properties, since children will be repopulated
+			// under unified param __children__ when converted into Geo Tree DND format
+			if (nodeType === 'Country') {
+				delete newNode.regions;
+			} else if (nodeType === 'Region') {
+				delete newNode.cities;
+			}
+
+			// ========= BEGIN Node Instance Methods ======== //
+			newNode.overrideChildWeights = function() {
+				var self = this;
+				self.__children__.forEach(function(node) {
+					node.weight = self.weight;
+					node.__overridden__ = true;
+					if (self.nodeType === 'Country') {
+						node.overrideChildWeights();
+					}
+				});
+			};
+			return newNode;
+		};
+
+		//====================================================//
+		//=============== BEGIN GeoTree Class ===============//
+		//====================================================//
+
+		/**
+		 * Subclass of DndTreeWrapper with GeoTree specific methods
+		 *
+		 * @param treeData shared tree data model with tree-dnd directive.
+		 * @param control tree-dnd control object. Will default to base methods when bound to directive, but
+		 *		can pass in an object containing custom control methods.
+		 * @param expanding_property tree-dnd `expanding property` model
+		 * @param columns tree-dnd column model
+		 * @constructor
+		 */
+		var GeoTree = function(treeData, control, expanding_property, columns) {
+			DndTreeWrapper.call(this, treeData, control, expanding_property, columns);	
+		};
+		GeoTree.prototype = Object.create(DndTreeWrapper.prototype);		
+
+		/**
+		 * Show all sub-geo within the selected geo node,
+		 * if selected geo node is a country, show all its regions/states
+		 * if selected geo node is a region, show all its cities
+		 *
+		 * @param node
+		 * @param callback
+		 */
+		GeoTree.prototype.expandGeoNode = function(node, callback) {
+			var flattened = this.traverseTree();
+			if (node.nodeType === 'Country') {
+				// Load regions/states for selected country dynamically from DB
+				Region.query({country: node._id}).$promise
+				.then(function(regions) {
+					regions.forEach(function(region) {
+						var newRegionNode = _initializeGeoTreeNode(region, 'Region', node._id);
+						flattened.push(newRegionNode);
+					});
+				});
+			} else if (node.nodeType === 'Region') {
+				// Load cities for selected region dynamicaly from DB
+				City.query({region: node._id}).$promise
+				.then(function(cities) {
+					cities.forEach(function(city) {
+						var newCityNode = _initializeGeoTreeNode(city, 'City', node._id);
+						flattened.push(newCityNode);
+					});
+				});
+			}
+			this.data = $TreeDnDConvert.line2tree(flattened, '_id', 'parentId');
+			callback(null, this.data);
+		};
+
+		/**
+		 * Show all sub-geo within the clicked geo node,
+		 * if clicked geo node is country, show all its regions,
+		 * if clicked geo node is region, show all its cities
+		 */
+		$scope.expandGeoNode = function(node) {
+			if (node.nodeType === 'Country') {
+				Region.query({country: node.target}).$promise.
+				then(function(response) {
+					$scope.countryNode = node;
+					if (response.length >= 1) {
+						$scope.geoTargets = [];
+						response.forEach(function(region) {
+							var newRegionNode = initializeGeoNode(region.name, 'Region', region._id);
+							$scope.geoTargets.push(newRegionNode);
+						});
+					}
+				});
+			} else if (node.nodeType === 'Region') {
+				City.query({region: node.target}).$promise.
+				then(function(response) {
+					$scope.regionNode = node;
+					if (response.length > 1) {
+						$scope.geoTargets = [];
+						response.forEach(function(city) {
+							var newCityNode = initializeGeoNode(city.name, 'City', city._id);
+							$scope.geoTargets.push(newCityNode);
+						});
+					}
+				});
+			}
 		};
 	}
 ]);
