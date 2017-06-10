0fbcf99eda66df11e81ada012040ae5565c69cf9
Chuoxian Yang <yangchuoxian.appdev@gmail.com>
2017-06-10 21:20:53 +0800

    GeoTree implementation and initialization(Not tested yet)

--- public/modules/advertiser/controllers/geoTargeting.client.controller.js
+++ public/modules/advertiser/controllers/geoTargeting.client.controller.js
@@ -2,8 +2,8 @@
 'use strict';
 
 angular.module('advertiser').controller('GeoTargetingController', [
-	'$scope', '$state', 'Notify', 'campaign', 'ngDialog', '$window', '$rootScope', 'Country', 'Region', 'City', 'DndTreeWrapper',
-	function($scope, $state, Notify, campaign, ngDialog, $window, $rootScope, Country, Region, City, DndTreeWrapper) {
+	'$scope', '$state', 'Notify', 'campaign', 'ngDialog', '$window', '$rootScope', 'Country', 'Region', 'City', 'DndTreeWrapper', '$TreeDnDConvert', 'OPENRTB', 'aggregationDateRanges', 'GeoAdStat', '$timeout',
+	function($scope, $state, Notify, campaign, ngDialog, $window, $rootScope, Country, Region, City, DndTreeWrapper, $TreeDnDConvert, OPENRTB, aggregationDateRanges, GeoAdStat, $timeout) {
 
 		$scope.Math = Math;
 		$scope.dirty = false;
@@ -307,36 +307,298 @@ angular.module('advertiser').controller('GeoTargetingController', [
 		};
 
 		/**
-		 * Show all sub-geo within the clicked geo node,
-		 * if clicked geo node is country, show all its regions,
-		 * if clicked geo node is region, show all its cities
+		 * Applies all applicable weight overrides from parents to children,
+		 * depending on if parent weights have changes from prior state. Meant to
+		 * only be used in $scope.$watch hook, where oldGeoTree is the older verion
+		 * of the GeoTree instance which has been modified.
+		 *
+		 * @param oldGeoTree old GeoTree instance in $scope
 		 */
-		$scope.expandGeoNode = function(node) {
-			if (node.nodeType === 'Country') {
-				Region.query({country: node.target}).$promise.
-				then(function(response) {
-					$scope.countryNode = node;
-					if (response.length >= 1) {
-						$scope.geoTargets = [];
-						response.forEach(function(region) {
-							var newRegionNode = initializeGeoNode(region.name, 'Region', region._id);
-							$scope.geoTargets.push(newRegionNode);
-						});
+		GeoTree.prototype.applyParentOverrides = function(oldGeoTree) {
+			var self = this;
+			function inner(newGeoTree, oldGeoTree) {
+				if (newGeoTree) {
+					for (var i = 0; i < newGeoTree.length; i ++) {
+						var newNode = newGeoTree[i];
+						var oldNode = oldGeoTree.length > 0 ? oldGeoTree[i] : {};
+						if (newNode && oldNode) {
+							if (newNode.weight !== oldNode.weight) {
+								newNode.overrideChildWeights();
+							}
+							inner(newNode.__children__, oldNode.__children__);
+						}
 					}
-				});
-			} else if (node.nodeType === 'Region') {
-				City.query({region: node.target}).$promise.
-				then(function(response) {
-					$scope.regionNode = node;
-					if (response.length > 1) {
-						$scope.geoTargets = [];
-						response.forEach(function(city) {
-							var newCityNode = initializeGeoNode(city.name, 'City', city._id);
-							$scope.geoTargets.push(newCityNode);
-						});
+				}
+			}
+			return inner(self.data, oldGeoTree.data);
+		};
+
+		/**
+		 * Sets __hideSlider__ property on this tree, given a masterTree
+		 * to perform diff on.
+		 *
+		 * ONLY USE IF YOU PLAN TO ALWAYS REMOVE NON-BASE LEVEL LEAF NODES FROM
+		 * MASTER TREE (I.E. NODES WITHOUT ANY CHILDREN)
+		 *
+		 * __hideSlider__ property is meant to indicate that the "weighting"
+		 * slider UI element should be hidden, which will prevent users from
+		 * settings a weight for this node.
+		 *
+		 * The logic encapsulated in this method dictates that sliders be hidden
+		 * for any nodes that exist in the provided "Master" GeoTree instance.
+		 * The KEY ASSUMPTION here is that once a Master tree node on longer has
+		 * any children, it is removed from the master tree.
+		 *
+		 * The `GeoTree.reGeoTree.prototype.moveNodeAndEmptyAncestors` method 
+		 * does exactly this, so as long as you use this method to clean up the
+		 * master tree when nodes are added to this tree, you're fine.
+		 *
+		 * @param masterTree master GeoTree instance w/ record of all nodes
+		 */
+		GeoTree.prototype.setSliderHiders = function(masterTree) {
+			// If you need to unhide a node slider, you also need to unhide
+			// all child node sliders, so this is just a quick method to hide
+			// node & all child sliders recursively
+			function unhideSliders(node) {
+				node.__hideSlider__ = false;
+				if (node.__children__ && node.__children__.length > 0) {
+					node.__children__.forEach(function(child) {
+						unhideSliders(child);
+					});
+				}
+			}
+
+			function inner(masterTree, slaveTree) {
+				slaveTree.forEach(function(node) {
+					var existingNode = _.find(masterTree, function(n) {
+						return n._id === node._id;
+					});
+					// If node exists in amster tree, seet hideSlider to true
+					// so user can't set weight on a node whose children are
+					// not all present
+					if (existingNode) {
+						node.__hideSlider__ = true;
+						if (node.__children__.length > 0) {
+							inner(existingNode.__children__, node.__children__);
+						}
+					} else {
+						unhideSliders(node);
 					}
 				});
 			}
+			return inner(masterTree.data, this.data);
+		};
+
+		//====================================================//
+		//================ END GeoTree Class =================//
+		//====================================================//
+		
+
+		//==========================================================//
+		//================ BEGIN GeoTree Instances =================//
+		//==========================================================//
+		var cpmColumnDef = {
+			field: 				'stats.cpm',
+			displayName: 		'Avg.CPM',
+			titleTemplate: 		'<a href="#" tooltip="Average CPM on this Geo area for the {{ dateRanges[defaultDateRange].label }}">'  +
+								'<i class="fa fa-line-chart"></i>&nbsp;Avg.<br/>CPM</a></div>',
+			titleClass: 		'wd-xxs text-center',
+			cellClass: 			'wd-xxs text-center',
+			cellTemplate: 		'<div ng-show="node.stats.cpm">{{ node.stats.cpm | currency: "$" : 2 }}</div>' +
+								'<small ng-hide="node.stats.cpm" class="text-muted"><i class="fa fa-heartbeat"> Not Enough Data</i></small>'
+
+		};
+		/**
+		 * Event Handler to be bound to each slider `onStart` event.
+		 * 
+		 * Sets __overridden__ param for node being changed to true, and
+		 * sets $scope.dirty to true as well.
+		 *
+		 * @param sliderId
+		 * @return {*}
+		 */
+		$scope.onStart = function(sliderId) {
+			function inner(tree) {
+				tree.forEach(function(node) {
+					if (node._id === sliderId) {
+						node.__overridden__ = false;
+					} else {
+						if (node.__children__ && node.__children__.length > 0) {
+							inner(node.__children__);
+						}
+					}
+				});	
+			}
+			$scope.dirty = true;
+			return inner($scope.geo_targets.data);
+		};
+
+		/**
+		 * Geo Targets tree vars
+		 */
+		$scope.geo_targets = new GeoTree([], 
+			{
+				remove: function(node) {
+					// Add whole ancestor branch to new tree, as necessary
+					GeoTree.prototype.moveNode($scope.geo_targets, null, node);
+					$scope.dirty = true;
+				}
+			},
+			{
+				field: 'name',
+				cellClass: 'v-middle wd-md',
+				displayName: 'Name',
+				logoSize: 'xs'	
+			},
+			[
+				{
+				    field: "weight",
+				    titleClass:  'text-center',
+				    displayName: "Weight",
+				    cellTemplate: '<rzslider rz-slider-model="node.weight" rz-slider-options="{floor: 0,ceil: Math.round(campaign.max_bid/campaign.base_bid * 10) / 10,step: 0.0001,precision: 4,id: node._id, showSelectionBar: true, onStart: onStart, hideLimitLabels: true}" ng-hide="node.__hideSlider__"></rzslider>' +
+				    '<div class="text-muted" ng-show="node.__hideSlider__ && !node.__expanded__"><small><i class="fa fa-plus-circle"></i><em>&nbsp;&nbsp;Expand to view & set bids</em></small></div>'
+				},
+				{
+				    field: 'bid',
+				    displayName: "Bid",
+				    titleClass:   'wd-xxs text-center',
+				    cellClass:    'wd-xxs text-center',
+				    cellTemplate: '<span ng-hide="node.__hideSlider__" ng-class="{ \'text-green\': Math.min(node.weight * campaign.base_bid, campaign.max_bid) > node.stats.cpm, \'text-warning\': Math.min(node.weight * campaign.base_bid, campaign.max_bid) < node.stats.cpm, \'text-danger\': Math.min(node.weight * campaign.base_bid, campaign.max_bid) < node.stats.cpm / 2 }">' +
+				    '<strong>{{ Math.min(node.weight * campaign.base_bid, campaign.max_bid) | currency : "$" : 2 }}</strong></span>'
+				},
+				cpmColumnDef,
+				{
+				    displayName:  'Actions',
+				    titleClass:   'wd-xxs text-center',
+				    cellClass:    'wd-xxs text-center',
+				    cellTemplate: '<button type="button" class="btn btn-xs bg-gray-light" ng-click="target_sites.control.remove(node)" tooltip="Clear Bids">' +
+				    '<i class="fa fa-lg fa-remove"></i></button>'
+				}
+			]
+		);
+
+		/**
+		 * GeoTree for blocked geo tree vars
+		 */
+		$scope.blocked_geos = new GeoTree([],
+			{
+				remove: function(node) {
+					GeoTree.prototype.moveNode($scope.blocked_geos, null, node);
+					$scope.dirty = true;
+				}
+			},
+			{
+			    field: "name",
+			    cellClass: 'v-middle',
+			    displayName: 'Name'
+			},
+			[{
+			    displayName:  'Actions',
+			    cellTemplate: '<button type="button" class="btn btn-xs bg-gray-light" ng-click="blocked_sites.control.remove(node)" tooltip="Unblock">' +
+			    '<i class="fa fa-lg fa-remove"></i></button>'
+			}]
+		);
+
+		//==========================================================//
+		//================= END GeoTree Instances =================//
+		//==========================================================//
+
+		$scope.positions = function(posCode) {
+			return _.find(OPENRTB.positions, function(pos_obj) {
+				return pos_obj.code === posCode;
+			});
+		};
+
+		/**
+		 * This scope watch handles overriding of child entity weights when 
+		 * parent is changed.
+		 */
+		$scope.$watch(function(scope) {
+			return scope.geo_targets;
+		}, function(newGeoTargets, oldGeoTargets) {
+			if (newGeoTargets.data.length > 0 && oldGeoTargets.data.length > 0) {
+				newGeoTargets.applyParentOverrides(oldGeoTargets);
+			}
+			$timeout(function() {
+				$scope.$broadcast('rzSliderForceRender');
+			});
+		}, true);
+
+		//======================================================================//
+		//================= BEGIN Tree Initialization Handlers =================//
+		//======================================================================//
+
+		$scope.dateRanges = aggregationDateRanges(user.tz);
+		$scope.defaultDateRange = '30d';
+		
+		/**
+		 * Quick helper function to calculate CPMs for grouped geoadstat data
+		 * @param groupedData
+		 * @return {{}}
+		 * @private
+		 */
+		function _getCpms(groupedData) {
+			var cpms = {};
+			for (var id in groupedData) {
+				if (groupedData.hasOwnProperty(id)) {
+					var imps = _.sumBy(groupedData[id], function(row) { return row.imps; });
+					var spend = _.sumBy(groupedData[id], function(row) { return row.spend; });
+					var cpm = spend / imps * 1000;
+					cpms[id] = {
+						imps: imps,
+						spend: spend,
+						cpm: cpm
+					};
+				}
+			}	
+			return cpms;
+		}
+
+		/**
+		 * Gets impression, spend & CPM totals for given date range from GeoAdStat,
+		 * groups by all Countries, Regions & Cities for efficient retrieval
+		 *
+		 * @param GeoTree
+		 * @param dateRange
+		 */
+		$scope.getGeoTreeStats = function(geoTree, dateRange) {
+			// exit if was passed empty geoTree
+			if (!geoTree || geoTree.length === 0) return;	
+
+			var startDate = $scope.dateRanges[dateRange].startDate;
+			var endDate = $scope.dateRanges[dateRange].endDate;
+			var geos = [];
+			geoTree.forEach(function(geo) {
+				geos.push(geo._id);
+			});
+			var geosQueryStr;
+			if (geos.length === 1) {
+				geosQueryStr = geos[0];
+			} else {
+				geosQueryStr = '{in}' + geos.join(',');
+			}
+			GeoAdStat.pubSummaryQuery({
+				groupBy: 'country,region,city',
+				country: geosQueryStr,
+				startDate: startDate,
+				endDate: endDate
+			}).then(function(response) {
+				var allGeosStats = {
+					Country: _getCpms(_.groupBy(response.data, '_id.country')),
+					Region: _getCpms(_.groupBy(response.data, '_id.region')),
+					City: _getCpms(_.groupBy(response.data, '_id.city'))
+				};
+				// Now bind to geoTree data to use in template
+				function inner(treeData) {
+					treeData.forEach(function(node) {
+						node.stats = allGeosStats[node.nodeType][node._id];
+						if (node.__children__ && node.__children__.length > 0) {
+							inner(node.__children__);
+						}
+					});
+				}
+				inner(geoTree);
+			});
 		};
 	}
 ]);
