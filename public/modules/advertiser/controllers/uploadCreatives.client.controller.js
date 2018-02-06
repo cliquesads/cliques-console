f614509d535444bef0032e5f6b35978ebd6d40bb
Ben Liang <bliang@cliquesads.com>
2018-02-06 17:38:16 -0500

    NF: Add sliders to manage creative UI and split up manage & upload controllers into separate files

--- public/modules/advertiser/controllers/uploadCreatives.client.controller.js
+++ public/modules/advertiser/controllers/uploadCreatives.client.controller.js
@@ -1,82 +1,7 @@
 /* global _, angular, moment */
 'use strict';
 
-angular.module('advertiser').controller('manageCreativesController', [
-    '$scope',
-    'campaign',
-    'AdvertiserUtils',
-    'FileUploader',
-    'ngDialog',
-    'Notify',
-    'NATIVE_SPECS',
-    function($scope, campaign,AdvertiserUtils,FileUploader,ngDialog, Notify, NATIVE_SPECS){
-        // Set form hidden by default
-        $scope.formVisible = false;
-        $scope.NATIVE_SPECS = NATIVE_SPECS;
-
-        /**
-         * Get Campaigns from URL state params on load
-         */
-        $scope.advertiser = campaign.advertiser;
-        $scope.campaignIndex = campaign.index;
-        $scope.campaign = campaign.campaign;
-
-        // almost trivial wrapper for scope.advertiser.$update that just ensures campaign
-        // is reset properly in scope when advertiser is updated, and sets scope.saveerror if error
-        // is thrown.
-        $scope.update = function(success, error){
-            this.advertiser.$update(function(response){
-                $scope.campaign = $scope.advertiser.campaigns[$scope.campaignIndex];
-                if (success) success(response);
-            },function(errorResponse){
-                if (error) error(errorResponse);
-                $scope.saveerror = errorResponse.data.message;
-            });
-        };
-
-        $scope.remove = function(creativegroup, creative){
-            ngDialog.openConfirm({
-                template:'\
-                            <p>Are you sure you want to delete this creative? This cannot be undone.</p>\
-                            <div class="ngdialog-buttons">\
-                                <button type="button" class="ngdialog-button ngdialog-button-secondary" ng-click="closeThisDialog(0)">No</button>\
-                                <button type="button" class="ngdialog-button ngdialog-button-primary" ng-click="confirm(1)">Yes</button>\
-                        </div>',
-                plain: true
-            }).then(function(val){
-                if (val === 1){
-                    var removedCreative;
-                    var removedCreativeGroup;
-                    // first find indices of desired creative
-                    var crg_ind = _.findIndex($scope.campaign.creativegroups, function(crg) { return crg === creativegroup; });
-                    var cr_ind = _.findIndex($scope.campaign.creativegroups[crg_ind].creatives, function(cr) { return cr === creative; });
-                    // remove from creatives document array
-                    removedCreative = $scope.campaign.creativegroups[crg_ind].creatives.splice(cr_ind, 1);
-                    //remove creative group if it doesn't contain any creatives anymore
-                    if ($scope.campaign.creativegroups[crg_ind].creatives.length === 0){
-                        removedCreativeGroup = $scope.campaign.creativegroups.splice(crg_ind, 1);
-                    }
-                    $scope.update(function(response){},
-                    function(errorResponse){
-                        // add back in if
-                        if (removedCreativeGroup){
-                            $scope.campaign.creativegroups.splice(crg_ind, 0, removedCreativeGroup[0]);
-                        }
-                        $scope.campaign.creativegroups[crg_ind].creatives.splice(cr_ind, 0, removedCreative[0]);
-                    });
-                }
-            });
-        };
-
-        $scope.addNewCreatives = function(){
-            ngDialog.open({
-                className: 'ngdialog-theme-default dialogwidth1000',
-                template: 'modules/advertiser/views/partials/upload-creatives.html',
-                controller: 'uploadCreativesController',
-                data: {advertiser: $scope.advertiser, campaign: $scope.campaign}
-            });
-        };
-}]).controller('uploadCreativesController', [
+angular.module('advertiser').controller('uploadCreativesController', [
     '$scope',
     'AdvertiserUtils',
     'FileUploader',
@@ -232,6 +157,4 @@ angular.module('advertiser').controller('manageCreativesController', [
                 $scope.closeThisDialog('Success');
             }, function(errorResponse){});
         };
-
-
-}]);
+    }]);
