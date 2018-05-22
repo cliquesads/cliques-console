1d4b87aba78cd7f79c455fbb83175dc7760b3f09
Ben Liang <bliang@cliquesads.com>
2018-05-22 00:54:14 -0400

    Add almost trivial template filter to handle toggle logic

--- public/modules/core/filters/filters.js
+++ public/modules/core/filters/filters.js
@@ -2,7 +2,7 @@
  * Created by bliang on 8/4/15.
  */
 
-/* global _, angular */
+/* global _, angular, pricing */
 'use strict';
 
 // This filter makes the assumption that the input will be in decimal form (i.e. 17% is 0.17).
@@ -16,5 +16,15 @@ angular.module('core').filter('percentage', ['$filter', function ($filter) {
         if (input!==null)
             input = input.toLowerCase();
         return input.substring(0,1).toUpperCase()+input.substring(1);
+    };
+})
+// filter to pull correct pricing metric from row input
+.filter('cpcOrCpm', function() {
+    return function(model) {
+        if (pricing === 'CPC'){
+            return model.cpc;
+        } else {
+            return model.cpm;
+        }
     };
 });
