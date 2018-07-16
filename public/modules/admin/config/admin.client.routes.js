8d8e24656fbee67c51a23f0620215719172f96cf
Ben Liang <bliang@cliquesads.com>
2018-07-16 10:34:53 -0400

    NF: Add org invites to all deployments

--- public/modules/admin/config/admin.client.routes.js
+++ public/modules/admin/config/admin.client.routes.js
@@ -27,43 +27,8 @@ angular.module('advertiser').config(['$stateProvider',
                     controller: 'PaymentAdminController'
                 }
             }
-        });
-        if (deploymentMode === 'adNetwork') {
-            $stateProvider.state('app.admin.listAccessCodes', {
-                url: '/admin/access-codes',
-                title: 'All Access Codes',
-                resolve: {
-                    $title: function () {
-                        return 'All Access Codes';
-                    }
-                },
-                views: {
-                    'main': {
-                        templateUrl: 'modules/accesscode/views/list-accesscode.client.view.html',
-                        controller: 'ListAccessCodeController'
-                    }
-                }
-            }).state('app.admin.listAccessCodes.viewAccessCode', {
-                url: '/admin/access-codes/:accessCodeId',
-                title: 'View Access Codes',
-                resolve: {
-                    accessCode: function (AccessCode, $stateParams) {
-                        return AccessCode.get({accessCodeId: $stateParams.accessCodeId}).$promise;
-
-                    },
-                    $title: function (accessCode) {
-                        return accessCode.code;
-                    }
-                },
-                views: {
-                    'main': {
-                        templateUrl: 'modules/accesscode/views/view-access-code.client.view.html',
-                        controller: 'ViewaccessLinkController'
-                    }
-                }
-            });
-        } else if (deploymentMode === 'contentNetwork') {
-            $stateProvider.state('app.admin.listAccessLinks', {
+        })
+        .state('app.admin.listAccessLinks', {
                 url: '/admin/access-link',
                 title: 'Organization Invites',
                 resolve: {
